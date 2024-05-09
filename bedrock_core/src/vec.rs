67bdbc7e12a5e81eaba5b0388b014194b8706763
Adrian <adrian.olbrich1@gmail.com>
2024-05-09 10:53:56 +0200

    add vecs

---
+++ bedrock_core/src/vec.rs
@@ -0,0 +1,12 @@
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct  Vec2(i32, i32);
+
+#[derive(Debug, Copy, Clone)]
+pub struct  Vec2f(f32, f32);
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct  Vec3(i32, i32, i32);
+
+#[derive(Debug, Copy, Clone)]
+pub struct  Vec3f(f32, f32, f32);
