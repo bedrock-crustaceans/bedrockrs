9a48aef7451478ac07bac075d1ab7ff96c544c20
Adrian <adrian.olbrich1@gmail.com>
2024-05-09 08:19:58 +0200

    added cache status

---
+++ protocol/src/packets/client_cache_status.rs
@@ -0,0 +1,6 @@
+use serialize_derive::{MCProtoDeserialize, MCProtoSerialize};
+
+#[derive(Debug, Copy, Clone, MCProtoSerialize, MCProtoDeserialize)]
+pub struct ClientCacheStatusPacket {
+    pub cache_supported: bool
+}
