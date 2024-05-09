2ceee5118492850f58d994af148b68410a94ac70
Adrian <adrian.olbrich1@gmail.com>
2024-05-09 10:59:16 +0200

    add vecs with named fields

--- bedrock_core/src/vec.rs
+++ bedrock_core/src/vec.rs
@@ -1,12 +1,12 @@
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct  Vec2(i32, i32);
+pub struct  Vec2{x: i32, z: i32}
 
 #[derive(Debug, Copy, Clone)]
-pub struct  Vec2f(f32, f32);
+pub struct  Vec2f{x: f32, z: f32}
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct  Vec3(i32, i32, i32);
+pub struct  Vec3{x: i32, y: i32, z: i32}
 
 #[derive(Debug, Copy, Clone)]
-pub struct  Vec3f(f32, f32, f32);
+pub struct  Vec3f{x: f32, y: f32, z: f32}
