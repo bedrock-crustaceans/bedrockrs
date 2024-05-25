57a988464bab842ca2815dcfaf2c134dfc3ac5bd
Adrian <adrian.olbrich1@gmail.com>
2024-05-25 22:23:07 +0200

    add inlining

--- nbt/src/endian/little_endian.rs
+++ nbt/src/endian/little_endian.rs
@@ -8,49 +8,56 @@ use crate::error::NbtError;
 pub struct NbtLittleEndian;
 
 impl NbtByteOrder for NbtLittleEndian {
-    fn write_u8(buf: &mut Vec<u8>, byte: u8) -> Result<(), NbtError> {
+    #[inline]
+fn write_u8(buf: &mut Vec<u8>, byte: u8) -> Result<(), NbtError> {
         match buf.write_u8(byte) {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn write_i16(buf: &mut Vec<u8>, int16: i16) -> Result<(), NbtError> {
+    #[inline]
+fn write_i16(buf: &mut Vec<u8>, int16: i16) -> Result<(), NbtError> {
         match buf.write_i16::<LittleEndian>(int16) {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn write_i32(buf: &mut Vec<u8>, int32: i32) -> Result<(), NbtError> {
+    #[inline]
+fn write_i32(buf: &mut Vec<u8>, int32: i32) -> Result<(), NbtError> {
         match buf.write_i32::<LittleEndian>(int32) {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn write_i64(buf: &mut Vec<u8>, int64: i64) -> Result<(), NbtError> {
+    #[inline]
+fn write_i64(buf: &mut Vec<u8>, int64: i64) -> Result<(), NbtError> {
         match buf.write_i64::<LittleEndian>(int64) {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn write_f32(buf: &mut Vec<u8>, float32: f32) -> Result<(), NbtError> {
+    #[inline]
+fn write_f32(buf: &mut Vec<u8>, float32: f32) -> Result<(), NbtError> {
         match buf.write_f32::<LittleEndian>(float32) {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn write_f64(buf: &mut Vec<u8>, float64: f64) -> Result<(), NbtError> {
+    #[inline]
+fn write_f64(buf: &mut Vec<u8>, float64: f64) -> Result<(), NbtError> {
         match buf.write_f64::<LittleEndian>(float64) {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn write_string(buf: &mut Vec<u8>, string: String) -> Result<(), NbtError> {
+    #[inline]
+fn write_string(buf: &mut Vec<u8>, string: String) -> Result<(), NbtError> {
         match Self::write_i16(
             buf,
             match string.len().try_into() {
@@ -70,49 +77,56 @@ impl NbtByteOrder for NbtLittleEndian {
         Ok(())
     }
 
-    fn read_u8(buf: &mut Cursor<Vec<u8>>) -> Result<u8, NbtError> {
+    #[inline]
+fn read_u8(buf: &mut Cursor<Vec<u8>>) -> Result<u8, NbtError> {
         match buf.read_u8() {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn read_i16(buf: &mut Cursor<Vec<u8>>) -> Result<i16, NbtError> {
+    #[inline]
+fn read_i16(buf: &mut Cursor<Vec<u8>>) -> Result<i16, NbtError> {
         match buf.read_i16::<LittleEndian>() {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn read_i32(buf: &mut Cursor<Vec<u8>>) -> Result<i32, NbtError> {
+    #[inline]
+fn read_i32(buf: &mut Cursor<Vec<u8>>) -> Result<i32, NbtError> {
         match buf.read_i32::<LittleEndian>() {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn read_i64(buf: &mut Cursor<Vec<u8>>) -> Result<i64, NbtError> {
+    #[inline]
+fn read_i64(buf: &mut Cursor<Vec<u8>>) -> Result<i64, NbtError> {
         match buf.read_i64::<LittleEndian>() {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn read_f32(buf: &mut Cursor<Vec<u8>>) -> Result<f32, NbtError> {
+    #[inline]
+fn read_f32(buf: &mut Cursor<Vec<u8>>) -> Result<f32, NbtError> {
         match buf.read_f32::<LittleEndian>() {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn read_f64(buf: &mut Cursor<Vec<u8>>) -> Result<f64, NbtError> {
+    #[inline]
+fn read_f64(buf: &mut Cursor<Vec<u8>>) -> Result<f64, NbtError> {
         match buf.read_f64::<LittleEndian>() {
             Ok(v) => Ok(v),
             Err(e) => Err(NbtError::IOError(e)),
         }
     }
 
-    fn read_string(buf: &mut Cursor<Vec<u8>>) -> Result<String, NbtError> {
+    #[inline]
+fn read_string(buf: &mut Cursor<Vec<u8>>) -> Result<String, NbtError> {
         let len = match Self::read_i16(buf) {
             Ok(v) => v,
             Err(e) => return Err(e),
