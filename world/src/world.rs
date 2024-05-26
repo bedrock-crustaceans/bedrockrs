8aaff9d10f44cfa1f247b5049ba3d55f303696ab
Finnbar M <xfinnbar@gmail.com>
2024-05-26 18:18:57 +0000

    fix and refactor

--- world/src/world.rs
+++ world/src/world.rs
@@ -1,78 +1,82 @@
-use std::{collections::HashMap, path::PathBuf};
 use mojang_leveldb::{error::DBError, Options, ReadOptions, WriteBatch, WriteOptions, DB};
 use nbt::{endian::little_endian::NbtLittleEndian, NbtTag};
+use std::{collections::HashMap, path::PathBuf};
 use uuid::Uuid;
 
 use crate::{error::WorldError, str_to_ascii_i8, vec_i8_into_u8, vec_u8_into_i8};
 
 pub struct World {
-    db: DB
+    db: DB,
 }
 
-const READ_OPTIONS: ReadOptions = ReadOptions{fill_cache: true, verify_checksums: true};
-const WRITE_OPTIONS: WriteOptions = WriteOptions{sync: true};
+const READ_OPTIONS: ReadOptions = ReadOptions {
+    fill_cache: true,
+    verify_checksums: true,
+};
+const WRITE_OPTIONS: WriteOptions = WriteOptions { sync: true };
 
 impl World {
     pub fn open(directory: PathBuf) -> Result<World, DBError> {
-        Ok(World { 
+        Ok(World {
             db: DB::open(
-                &directory.join("db").display().to_string(), 
-                Options{
-                    compression: mojang_leveldb::CompressionType::ZlibRaw, 
-                    create_if_missing: true}
-            )?
+                &directory.join("db").display().to_string(),
+                Options {
+                    compression: mojang_leveldb::CompressionType::ZlibRaw,
+                    create_if_missing: true,
+                },
+            )?,
         })
     }
 
     pub fn get_player(&self, uuid: Uuid) -> Result<Option<HashMap<String, NbtTag>>, WorldError> {
+        let mut str = uuid.to_string();
+        str.insert_str(0, "player_");
 
-        let mut str = uuid.to_string(); 
-        str.insert_str(0, "player_"); 
-
-        let res = self.db.get(
-            READ_OPTIONS, 
-            str_to_ascii_i8(&str).unwrap().as_slice()
-        );
-
-        if let Ok(Some(bytes)) = res {
-            let u8_bytes = vec_i8_into_u8(bytes.get().into());
-            let res = NbtTag::nbt_deserialize_vec::<NbtLittleEndian>(u8_bytes);
-            if let Ok(tag) = res {
-                if let NbtTag::Compound(ctag) = tag.1 {
-                    return Ok(Some(ctag));
-                }  
-            } else if let Err(e) = res {
-                return Err(WorldError::NbtError(e));
-            }
-        } else if let Err(x) = res {
-            return Err(WorldError::DBError(x));
+        match self
+            .db
+            .get(READ_OPTIONS, str_to_ascii_i8(&str).unwrap().as_slice())
+        {
+            Ok(maybe_bytes) => match maybe_bytes {
+                Some(bytes) => {
+                    let u8_bytes = vec_i8_into_u8(bytes.get().into());
+                    match NbtTag::nbt_deserialize_vec::<NbtLittleEndian>(u8_bytes) {
+                        Ok((_, tag)) => {
+                            match tag {
+                                NbtTag::Compound(ctag) => Ok(Some(ctag)),
+                                _ => Err(WorldError::FormatError("Player data tag is not a compound tag".to_string()))
+                            }
+                        },
+                        Err(e) => Err(WorldError::NbtError(e))
+                    }
+                },
+                None => Ok(None)
+            },
+            Err(e) => Err(WorldError::DBError(e))
         }
-
-        Ok(None)
-
     }
 
-    pub fn set_player(&mut self, uuid: Uuid, tag: NbtTag) -> Result<(), WorldError> {
+    pub fn set_player(&mut self, uuid: Uuid, data: HashMap<String, NbtTag>) -> Result<(), WorldError> {
+        let tag = NbtTag::Compound(data);
         match tag.nbt_serialize_vec::<NbtLittleEndian>("") {
             Ok(sertag) => {
                 let byte_nbt = vec_u8_into_i8(sertag);
 
-                let mut str = uuid.to_string(); 
-                str.insert_str(0, "player_"); 
+                let mut str = uuid.to_string();
+                str.insert_str(0, "player_");
 
                 let mut wb = WriteBatch::new();
 
-                wb.put(str_to_ascii_i8(&str).unwrap().as_slice(), byte_nbt.as_slice());
+                wb.put(
+                    str_to_ascii_i8(&str).unwrap().as_slice(),
+                    byte_nbt.as_slice(),
+                );
 
                 match self.db.write(WRITE_OPTIONS, wb) {
                     Ok(()) => Ok(()),
-                    Err(dberr) => Err(WorldError::DBError(dberr))
+                    Err(dberr) => Err(WorldError::DBError(dberr)),
                 }
-            },
-            Err(e) => {
-                Err(WorldError::NbtError(e))
             }
+            Err(e) => Err(WorldError::NbtError(e)),
         }
-        
     }
 }
