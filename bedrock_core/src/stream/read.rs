7d1a61fe06f1bdbeb6cf3f34f2aadcbb83ef75ed
Adrian <adrian.olbrich1@gmail.com>
2024-05-31 21:52:17 +0200

    add stream

---
+++ bedrock_core/src/stream/read.rs
@@ -0,0 +1,260 @@
+use std::io::Read;
+use bytes::{Buf, Bytes, BytesMut};
+use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
+use bytes::buf::Reader;
+use varint_rs::VarintReader;
+use crate::*;
+
+/// A wrapper around [`bytes::Bytes`].
+/// (A cheaply cloneable and sliceable chunk of contiguous memory).
+#[repr(transparent)]
+pub struct ByteStreamRead(Reader<Bytes>);
+
+impl ByteStreamRead {
+    /// Creates a new empty `ByteStreamRead`.
+    /// This will not allocate and the returned Bytes handle will be empty.
+    #[inline]
+    pub fn new() -> Self {
+        Self(Bytes::new().reader())
+    }
+
+    /// Creates a new empty `ByteStreamRead` from a given [`bytes::Bytes`] object.
+    #[inline]
+    pub fn from_bytes(bytes: Bytes) -> Self {
+        Self(bytes.reader())
+    }
+
+    /// Creates a new `ByteStreamRead` from a static slice.
+    #[inline]
+    pub fn from_static(bytes: &'static [u8]) -> Self {
+        Self(Bytes::from_static(bytes).reader())
+    }
+
+    /// Returns the number of bytes contained in this `ByteStreamRead`.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.0.get_ref().len()
+    }
+
+    /// Returns true if the `ByteStreamRead` has a length of 0.
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.0.get_ref().is_empty()
+    }
+
+    /// Clears the buffer, removing all data. Existing capacity is preserved.
+    #[inline]
+    pub fn clear(&mut self) {
+        self.0.get_mut().clear();
+    }
+
+    /// Read an u8
+    #[inline]
+    pub fn read_u8(&mut self) -> Result<u8, std::io::Error> {
+        self.0.read_u8()
+    }
+
+    /// Read an i8
+    #[inline]
+    pub fn read_i8(&mut self) -> Result<i8, std::io::Error> {
+        self.0.read_i8()
+    }
+
+    /// Read an u16le
+    #[inline]
+    pub fn read_u16le(&mut self) -> Result<u16le, std::io::Error> {
+        match self.0.read_u16::<LittleEndian>() {
+            Ok(v) => { Ok(u16le(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an u16be
+    #[inline]
+    pub fn read_u16be(&mut self) -> Result<u16be, std::io::Error> {
+        match self.0.read_u16::<BigEndian>() {
+            Ok(v) => { Ok(u16be(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an i16le
+    #[inline]
+    pub fn read_i16le(&mut self) -> Result<i16le, std::io::Error> {
+        match self.0.read_i16::<LittleEndian>() {
+            Ok(v) => { Ok(i16le(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an i16be
+    #[inline]
+    pub fn read_i16be(&mut self) -> Result<i16be, std::io::Error> {
+        match self.0.read_i16::<BigEndian>() {
+            Ok(v) => { Ok(i16be(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an u32le
+    #[inline]
+    pub fn read_u32le(&mut self) -> Result<u32le, std::io::Error> {
+        match self.0.read_u32::<LittleEndian>() {
+            Ok(v) => { Ok(u32le(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an u32be
+    #[inline]
+    pub fn read_u32be(&mut self) -> Result<u32be, std::io::Error> {
+        match self.0.read_u32::<BigEndian>() {
+            Ok(v) => { Ok(u32be(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an i32le
+    #[inline]
+    pub fn read_i32le(&mut self) -> Result<i32le, std::io::Error> {
+        match self.0.read_i32::<LittleEndian>() {
+            Ok(v) => { Ok(i32le(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an i32be
+    #[inline]
+    pub fn read_i32be(&mut self) -> Result<i32be, std::io::Error> {
+        match self.0.read_i32::<BigEndian>() {
+            Ok(v) => { Ok(i32be(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an u64le
+    #[inline]
+    pub fn read_u64le(&mut self) -> Result<u64le, std::io::Error> {
+        match self.0.read_u64::<LittleEndian>() {
+            Ok(v) => { Ok(u64le(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an u64be
+    #[inline]
+    pub fn read_u64be(&mut self) -> Result<u64be, std::io::Error> {
+        match self.0.read_u64::<BigEndian>() {
+            Ok(v) => { Ok(u64be(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an i64le
+    #[inline]
+    pub fn read_i64le(&mut self) -> Result<i64le, std::io::Error> {
+        match self.0.read_i64::<LittleEndian>() {
+            Ok(v) => { Ok(i64le(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an i64be
+    #[inline]
+    pub fn read_i64be(&mut self) -> Result<i64be, std::io::Error> {
+        match self.0.read_i64::<BigEndian>() {
+            Ok(v) => { Ok(i64be(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an u128le
+    #[inline]
+    pub fn read_u128le(&mut self) -> Result<u128le, std::io::Error> {
+        match self.0.read_u128::<LittleEndian>() {
+            Ok(v) => { Ok(u128le(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an u128be
+    #[inline]
+    pub fn read_u128be(&mut self) -> Result<u128be, std::io::Error> {
+        match self.0.read_u128::<BigEndian>() {
+            Ok(v) => { Ok(u128be(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an i128le
+    #[inline]
+    pub fn read_i128le(&mut self) -> Result<i128le, std::io::Error> {
+        match self.0.read_i128::<LittleEndian>() {
+            Ok(v) => { Ok(i128le(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an i128be
+    #[inline]
+    pub fn read_i128be(&mut self) -> Result<i128be, std::io::Error> {
+        match self.0.read_i128::<BigEndian>() {
+            Ok(v) => { Ok(i128be(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an uvar32
+    #[inline]
+    pub fn read_uvar32(&mut self) -> Result<uvar32, std::io::Error> {
+        match self.0.read_u32_varint() {
+            Ok(v) => { Ok(uvar32(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an ivar32
+    #[inline]
+    pub fn read_ivar32(&mut self) -> Result<ivar32, std::io::Error> {
+        match self.0.read_i32_varint() {
+            Ok(v) => { Ok(ivar32(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an uvar64
+    #[inline]
+    pub fn read_uvar64(&mut self) -> Result<uvar64, std::io::Error> {
+        match self.0.read_u64_varint() {
+            Ok(v) => { Ok(uvar64(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read an ivar64
+    #[inline]
+    pub fn read_ivar64(&mut self) -> Result<ivar64, std::io::Error> {
+        match self.0.read_i64_varint() {
+            Ok(v) => { Ok(ivar64(v)) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read a f32
+    #[inline]
+    pub fn read_f32(&mut self) -> Result<f32, std::io::Error> {
+        match self.0.read_f32::<LittleEndian>() {
+            Ok(v) => { Ok(v) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Read a f64
+    #[inline]
+    pub fn read_f64(&mut self) -> Result<f64, std::io::Error> {
+        match self.0.read_f64::<BigEndian>() {
+            Ok(v) => { Ok(v) }
+            Err(e) => { Err(e) }
+        }
+    }
+}
---
+++ bedrock_core/src/stream/write.rs
@@ -0,0 +1,272 @@
+use byteorder::{BigEndian, LittleEndian, ReadBytesExt, WriteBytesExt};
+use bytes::{Buf, BufMut, BytesMut};
+use bytes::buf::Writer;
+use varint_rs::{VarintReader, VarintWriter};
+use crate::stream::read::ByteStreamRead;
+use crate::{i128be, i128le, i16be, i16le, i32be, i32le, i64be, i64le, ivar32, ivar64, u128be, u128le, u16be, u16le, u32be, u32le, u64be, u64le, uvar32, uvar64};
+
+/// A wrapper around [`bytes::BytesMut`].
+/// (A unique reference to a contiguous slice of memory).
+#[repr(transparent)]
+pub struct ByteStreamWrite(Writer<BytesMut>);
+
+impl ByteStreamWrite {
+    /// Creates a new `ByteStreamWrite` with default capacity.
+    /// Resulting object has length 0 and unspecified capacity. This function does not allocate.
+    #[inline]
+    pub fn new() -> Self {
+        Self(BytesMut::new().writer())
+    }
+
+    /// Creates a new empty `ByteStreamWrite` from a given [`bytes::BytesMut`] object.
+    #[inline]
+    pub fn from_bytes_mut(bytes: BytesMut) -> Self {
+        Self(bytes.writer())
+    }
+
+    /// Returns the number of bytes contained in this `ByteStreamWrite`.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.0.get_ref().len()
+    }
+
+    /// Returns true if the `ByteStreamWrite` has a length of 0.
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.0.get_ref().is_empty()
+    }
+
+    /// Returns the number of bytes the `ByteStreamWrite` can hold without reallocating.
+    #[inline]
+    pub fn capacity(&self) -> usize {
+        self.0.get_ref().capacity()
+    }
+
+    /// Converts self into an immutable `ByteStreamWrite`.
+    #[inline]
+    pub fn freeze(self) -> ByteStreamRead {
+        ByteStreamRead::from_bytes(self.0.into_inner().freeze())
+    }
+
+    /// Creates a new `ByteStreamWrite`, which is initialized with zero.
+    #[inline]
+    pub fn zeroed(len: usize) -> Self {
+        Self(BytesMut::zeroed(len).writer())
+    }
+
+    /// Clears the buffer, removing all data. Existing capacity is preserved.
+    #[inline]
+    pub fn clear(&mut self) {
+        self.0.get_mut().clear();
+    }
+
+    /// Write an u8
+    #[inline]
+    pub fn write_u8(&mut self, n: u8) -> Result<(), std::io::Error> {
+        self.0.write_u8(n)
+    }
+
+    /// Write an i8
+    #[inline]
+    pub fn write_i8(&mut self, n: i8) -> Result<(), std::io::Error> {
+        self.0.write_i8(n)
+    }
+
+    /// Write an u16le
+    #[inline]
+    pub fn write_u16le(&mut self, n: u16le) -> Result<(), std::io::Error> {
+        match self.0.write_u16::<LittleEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an u16be
+    #[inline]
+    pub fn write_u16be(&mut self, n: u16be) -> Result<(), std::io::Error> {
+        match self.0.write_u16::<BigEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an i16le
+    #[inline]
+    pub fn write_i16le(&mut self, n: i16le) -> Result<(), std::io::Error> {
+        match self.0.write_i16::<LittleEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an i16be
+    #[inline]
+    pub fn write_i16be(&mut self, n: i16be) -> Result<(), std::io::Error> {
+        match self.0.write_i16::<BigEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an u32le
+    #[inline]
+    pub fn write_u32le(&mut self, n: u32le) -> Result<(), std::io::Error> {
+        match self.0.write_u32::<LittleEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an u32be
+    #[inline]
+    pub fn write_u32be(&mut self, n: u32be) -> Result<(), std::io::Error> {
+        match self.0.write_u32::<BigEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an i32le
+    #[inline]
+    pub fn write_i32le(&mut self, n: i32le) -> Result<(), std::io::Error> {
+        match self.0.write_i32::<LittleEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an i32be
+    #[inline]
+    pub fn write_i32be(&mut self, n: i32be) -> Result<(), std::io::Error> {
+        match self.0.write_i32::<BigEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an u64le
+    #[inline]
+    pub fn write_u64le(&mut self, n: u64le) -> Result<(), std::io::Error> {
+        match self.0.write_u64::<LittleEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an u64be
+    #[inline]
+    pub fn write_u64be(&mut self, n: u64be) -> Result<(), std::io::Error> {
+        match self.0.write_u64::<BigEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an i64le
+    #[inline]
+    pub fn write_i64le(&mut self, n: i64le) -> Result<(), std::io::Error> {
+        match self.0.write_i64::<LittleEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an i64be
+    #[inline]
+    pub fn write_i64be(&mut self, n: i64be) -> Result<(), std::io::Error> {
+        match self.0.write_i64::<BigEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an u128le
+    #[inline]
+    pub fn write_u128le(&mut self, n: u128le) -> Result<(), std::io::Error> {
+        match self.0.write_u128::<LittleEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an u128be
+    #[inline]
+    pub fn write_u128be(&mut self, n: u128be) -> Result<(), std::io::Error> {
+        match self.0.write_u128::<BigEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an i128le
+    #[inline]
+    pub fn write_i128le(&mut self, n: i128le) -> Result<(), std::io::Error> {
+        match self.0.write_i128::<LittleEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an i128be
+    #[inline]
+    pub fn write_i128be(&mut self, n: i128be) -> Result<(), std::io::Error> {
+        match self.0.write_i128::<BigEndian>(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an uvar32
+    #[inline]
+    pub fn write_uvar32(&mut self, n: uvar32) -> Result<(), std::io::Error> {
+        match self.0.write_u32_varint(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an ivar32
+    #[inline]
+    pub fn write_ivar32(&mut self, n: ivar32) -> Result<(), std::io::Error> {
+        match self.0.write_i32_varint(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an uvar64
+    #[inline]
+    pub fn write_uvar64(&mut self, n: uvar64) -> Result<(), std::io::Error> {
+        match self.0.write_u64_varint(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write an ivar64
+    #[inline]
+    pub fn write_ivar64(&mut self, n: ivar64) -> Result<(), std::io::Error> {
+        match self.0.write_i64_varint(n.0) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write a f32
+    #[inline]
+    pub fn write_f32(&mut self, n: f32) -> Result<(), std::io::Error> {
+        match self.0.write_f32::<LittleEndian>(n) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+
+    /// Write a f64
+    #[inline]
+    pub fn write_f64(&mut self, n: f64) -> Result<(), std::io::Error> {
+        match self.0.write_f64::<BigEndian>(n) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+}
