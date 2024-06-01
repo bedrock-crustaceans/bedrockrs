90db98906bd1926a8e148c1a5f3d0c9518216878
Adrian <adrian.olbrich1@gmail.com>
2024-06-01 23:37:38 +0200

    change name

--- proto_core/src/lib.rs
+++ proto_core/src/lib.rs
@@ -7,7 +7,7 @@ pub mod error;
 pub mod types;
 
 pub trait ProtoCodec: Sized {
-    fn proto_serialize(&self, buf: &mut ByteStreamWrite) -> Result<(), ProtoCodecError>;
+    fn proto_serialize(&self, stream: &mut ByteStreamWrite) -> Result<(), ProtoCodecError>;
 
-    fn proto_deserialize(cursor: &mut ByteStreamRead) -> Result<Self, ProtoCodecError>;
+    fn proto_deserialize(stream: &mut ByteStreamRead) -> Result<Self, ProtoCodecError>;
 }
