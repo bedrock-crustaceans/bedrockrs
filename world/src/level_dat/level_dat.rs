37e9eb645ffc7feff6ecc7bb49b86b34d96b1cc7
Adrian <adrian.olbrich1@gmail.com>
2024-06-01 15:37:16 +0200

    fix

--- world/src/level_dat/level_dat.rs
+++ world/src/level_dat/level_dat.rs
@@ -19,16 +19,21 @@ pub struct LevelDat {
     /// Specifies the name of the world. (NBT entry: `LevelName`)
     pub level_name: String,
 
+    /// Version of Bedrock Edition Storage Tool, currently is 10. (NBT entry: `StorageVersion`)
+    pub format_version: i32,
+
     /// The default permissions for players in the world. (NBT entry: `abilities`)
     pub abilities: WorldAbilities,
 
-    /// A key value pair map for each experiment that Minecraft has/had. (NBT entry: `experiments`)
+    /// A key value pair map for each experiment that Minecraft has/had.
+    /// It is impossible to parse all Experiments due to frequent changes.
+    /// (NBT entry: `experiments`)
     pub experiments: HashMap<String, bool>,
 
     /// The current difficulty setting. (NBT entry: `Difficulty`)
     pub difficulty: Difficulty,
     /// The dimension the player is in. (NBT entry: `Dimension`)
-    pub dimension: Dimension,
+    pub dimension: Option<Dimension>,
 
     /// If the bonus chest is enabled. (NBT entry: `bonusChestEnabled`)
     pub bonus_chest_enabled: bool,
@@ -53,11 +58,14 @@ pub struct LevelDat {
     /// The Z coordinate of the player's spawn position. Defaults to 0.
     /// (NBT entry: `SpawnZ`)
     pub spawn_z: i32,
+
+    /// If cheats are on. (NBT entry: `commandsEnabled`)
+    pub cheats: bool,
 }
 
 impl LevelDat {
     pub fn open(directory: &PathBuf) -> Result<(i32, i32, Self), WorldError> {
-        let mut file = match File::open(directory.join("db")) {
+        let mut file = match File::open(directory.join("level.dat")) {
             Ok(v) => { v }
             Err(e) => { return Err(WorldError::FormatError(e.to_string())) }
         };
@@ -120,6 +128,14 @@ impl LevelDat {
             }
         }
 
+        fn get_int32_option(map: &mut HashMap<String, NbtTag>, key: &str) -> Result<Option<i32>, WorldError> {
+            match map.remove(key) {
+                Some(NbtTag::Int32(v)) => Ok(Some(v)),
+                Some(other) => Err(WorldError::FormatError(format!("Expected `{}` in LevelDat to be of type Int32, got {:?}", key, other))),
+                None => Ok(None),
+            }
+        }
+
         fn get_int64(map: &mut HashMap<String, NbtTag>, key: &str) -> Result<i64, WorldError> {
             match map.remove(key) {
                 Some(NbtTag::Int64(v)) => Ok(v),
@@ -139,28 +155,37 @@ impl LevelDat {
         match tag {
             NbtTag::Compound(mut map) => Ok(Self {
                 level_name: get_string(&mut map, "LevelName")?,
-                abilities: WorldAbilities::parse(get_compound(&mut map, "abilities")?),
+                format_version: get_int32(&mut map, "StorageVersion")?,
+                abilities: WorldAbilities::parse(match map.remove("abilities") {
+                    Some(v) => { v }
+                    None => { Err(WorldError::FormatError(format!("Missing field `abilities` in LevelDat")))? }
+                })?,
                 experiments: {
-                    let nbt = get_compound(&mut map, "experiments")?;
-                    nbt.iter()
-                        .map(|(name, _)| {
-                            let value = get_byte_as_bool(&mut nbt.clone(), name)?; // cloning to avoid borrowing issues
-                            Ok((name.clone(), value))
-                        })
-                        .collect()
+                    let mut nbt = get_compound(&mut map, "experiments")?;
+                    let mut experiments = HashMap::new();
+
+                    for (name, tag) in nbt.iter() {
+                        experiments.insert(name.clone(), match tag {
+                            NbtTag::Byte(v) => { *v != 0 }
+                            other => { Err(WorldError::FormatError(format!("Expected `{}` in LevelDat experiments to be of type Byte, got {:?}", name, other)))? }
+                        });
+                    }
+
+                    experiments
                 },
                 difficulty: match get_int32(&mut map, "Difficulty")? {
                     0 => { Difficulty::Peaceful }
                     1 => { Difficulty::Easy }
                     2 => { Difficulty::Normal }
                     3 => { Difficulty::Hard }
-                    other => { Err(format!("Value for `Difficulty` is out of bounds, got {:?}", other))? }
+                    other => { Err(WorldError::FormatError(format!("Value for `Difficulty` is out of bounds, got {:?}", other)))? }
                 },
-                dimension: match get_int32(&mut map, "Dimension")? {
-                    0 => { Dimension::Overworld }
-                    1 => { Dimension::Nether }
-                    2 => { Dimension::End }
-                    other => { Err(format!("Value for `Dimension` is out of bounds, got {:?}", other))? }
+                dimension: match get_int32_option(&mut map, "Dimension")? {
+                    Some(1) => { Some(Dimension::Overworld) }
+                    Some(2) => { Some(Dimension::Nether) }
+                    Some(3) => { Some(Dimension::End) }
+                    Some(other) => { Err(WorldError::FormatError(format!("Value for `Dimension` is out of bounds, got {:?}", other)))? }
+                    None => { None }
                 },
                 bonus_chest_enabled: get_byte_as_bool(&mut map, "bonusChestEnabled")?,
                 bonus_chest_spawned: get_byte_as_bool(&mut map, "bonusChestSpawned")?,
@@ -169,6 +194,7 @@ impl LevelDat {
                 spawn_x: get_int32(&mut map, "SpawnX")?,
                 spawn_y: get_int32(&mut map, "SpawnY")?,
                 spawn_z: get_int32(&mut map, "SpawnZ")?,
+                cheats: get_byte_as_bool(&mut map, "commandsEnabled")?,
             }),
             other => Err(WorldError::FormatError(format!("Expected root tag in LevelDat to be of type Compound, got {:?}", other))),
         }
