affc01b70440234844ae6e1c3f80c557a46d3447
Adrian <adrian.olbrich1@gmail.com>
2024-06-01 15:36:51 +0200

    specify to use dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,33 @@
 pub mod core {
-    pub use bedrock_core::*;
+    pub use ::bedrock_core::*;
 }
 
 pub mod proto {
-    pub use proto::*;
+    pub use ::proto::*;
 
     pub mod codec {
-        pub use proto_core::error::ProtoCodecError;
-        pub use proto_core::ProtoCodec;
-        pub use proto_derive::ProtoCodec;
+        pub use ::proto_core::error::ProtoCodecError;
+        pub use ::proto_core::ProtoCodec;
+        pub use ::proto_derive::ProtoCodec;
     }
 }
 
 pub mod nbt {
-    pub use nbt::byte_order::*;
-    pub use nbt::endian::*;
-    pub use nbt::error::*;
-    pub use nbt::*;
+    pub use ::nbt::byte_order::*;
+    pub use ::nbt::endian::*;
+    pub use ::nbt::error::*;
+    pub use ::nbt::*;
 }
 
 pub mod packs {
-    pub use packs::*;
+    pub use ::packs::*;
 }
 
 #[cfg(feature = "world")]
 pub mod world {
-    pub use world::*;
+    pub use ::world::*;
 }
 
 pub mod form {
-    pub use form::*;
+    pub use ::form::*;
 }
