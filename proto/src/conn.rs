4f3852f08e3acd37057e47379a65ffffb890bbe7
Adrian <adrian.olbrich1@gmail.com>
2024-06-03 19:16:12 +0200

    refactor to new transport layer system

--- proto/src/conn.rs
+++ proto/src/conn.rs
@@ -1,224 +1,149 @@
-use std::io::{Cursor, Write};
+use std::io::{Cursor, Error, Write};
+use bedrock_core::stream::read::ByteStreamRead;
+use bedrock_core::stream::write::ByteStreamWrite;
+use tokio::io::AsyncWriteExt;
 
-use byteorder::{ReadBytesExt, WriteBytesExt};
-use rak_rs::connection::Connection as RakConnection;
+use crate::compression::Compression;
 
-use crate::compression::none::NoCompression;
-use crate::compression::snappy::SnappyCompression;
-use crate::compression::zlib::ZlibCompression;
-use crate::compression::{CompressionMethod, CompressionMethods};
 use crate::encryption::Encryption;
-use crate::error::ConnectionError;
+use crate::error::{CompressionError, ConnectionError};
 use crate::gamepacket::GamePacket;
-use crate::info::RAKNET_GAME_PACKET_ID;
+use crate::transport_layer::TransportLayerConn;
 
-pub struct Connection {
-    rak_connection: RakConnection,
-    compression: Option<CompressionMethods>,
-    encryption: Option<Encryption>,
+pub struct Conn {
+    /// Represents the connections internal transport layer, this allows using different
+    /// transport layers with the client or proxies, this can improve performance.
+    connection: TransportLayerConn,
+    /// Represents the connections' compression, the compression gets initialized in the
+    /// login process.
+    pub compression: Option<Compression>,
+    /// Represents the connections encryption, the encryption gets initialized in the
+    /// login process, if encryption is allowed.
+    pub encryption: Option<Encryption>,
 }
 
-impl Connection {
-    pub fn new(conn: RakConnection) -> Self {
+impl Conn {
+    pub fn new(conn: TransportLayerConn) -> Self {
         Self {
-            rak_connection: conn,
+            connection: conn,
             compression: None,
             encryption: None,
         }
     }
 
-    pub fn set_compression_method(&mut self, compression_method: Option<CompressionMethods>) {
-        self.compression = compression_method
-    }
-
-    pub fn get_compression_method(&self) -> &Option<CompressionMethods> {
-        &self.compression
-    }
-
     pub async fn send_gamepackets(
-        &self,
+        &mut self,
         game_packets: Vec<GamePacket>,
     ) -> Result<(), ConnectionError> {
-        let mut buf_pks = vec![];
+        let mut pk_stream = ByteStreamWrite::new();
 
         // Batch all game packets together
         for game_packet in game_packets {
-            // Write game packet
-            match game_packet.pk_serialize(&mut buf_pks) {
+            // Write a game packet
+            match game_packet.pk_serialize(&mut pk_stream) {
                 Ok(_) => {}
                 Err(e) => return Err(ConnectionError::ProtoCodecError(e)),
             }
         }
 
-        let mut buf = vec![];
-
-        match buf.write_u8(RAKNET_GAME_PACKET_ID) {
-            Ok(_) => {}
-            Err(_) => return Err(ConnectionError::WriteIOError),
-        };
-
         // Compress the data depending on compression method
-        if let Some(compression) = &self.compression {
-            match compression {
-                CompressionMethods::Zlib(zlib) => {
-                    // If the data is more than the compression threshold or equal to it
-                    // then compress it, else don't
-                    if buf_pks.len() as u64 >= zlib.threshold as u64 {
-                        // Write compression id for zlib
-                        match buf.write_u8(ZlibCompression::ID_u8) {
-                            Ok(_) => {}
-                            Err(_) => return Err(ConnectionError::WriteIOError),
-                        };
+        let compressed_stream = match &self.compression {
+            Some(compression) => {
+                let mut compressed_stream = ByteStreamWrite::new();
 
-                        // Decompress using zlib
-                        buf_pks = match zlib.compress(buf_pks) {
-                            Ok(v) => v,
-                            Err(e) => return Err(ConnectionError::CompressError(e)),
-                        };
-                    } else {
-                        // Write compression id for no compression
-                        match buf.write_u8(NoCompression::ID_u8) {
-                            Ok(_) => {}
-                            Err(_) => return Err(ConnectionError::WriteIOError),
-                        };
+                match compressed_stream.write_u8(compression.id_u8()) {
+                    Ok(_) => {}
+                    Err(e) => return Err(ConnectionError::IOError(e)),
+                };
+
+                if compression.compression_needed() && pk_stream.len() as u16 > compression.threshold() {
+                    match compression.compress(&pk_stream.freeze(), &mut compressed_stream) {
+                        Ok(_) => { }
+                        Err(e) => { return Err(ConnectionError::CompressError(e)) }
                     };
                 }
-                CompressionMethods::Snappy(snappy) => {
-                    // If the data is more than the compression threshold or equal to it
-                    // then compress it, else don't
-                    if buf_pks.len() as u64 >= snappy.threshold as u64 {
-                        // Write compression id for snappy
-                        match buf.write_u8(SnappyCompression::ID_u8) {
-                            Ok(_) => {}
-                            Err(_) => return Err(ConnectionError::WriteIOError),
-                        };
-
-                        // Decompress using Snappy
-                        buf_pks = match snappy.compress(buf_pks) {
-                            Ok(v) => v,
-                            Err(e) => return Err(ConnectionError::CompressError(e)),
-                        };
-                    } else {
-                        // Write compression id for no compression
-                        match buf.write_u8(NoCompression::ID_u8) {
-                            Ok(_) => {}
-                            Err(_) => return Err(ConnectionError::WriteIOError),
-                        };
-                    };
-                }
-                CompressionMethods::None => {
-                    // Write compression id
-                    match buf.write_u8(NoCompression::ID_u8) {
+                else {
+                    match compressed_stream.write(pk_stream.as_slice()) {
                         Ok(_) => {}
-                        Err(_) => return Err(ConnectionError::WriteIOError),
-                    };
+                        Err(e) => { return Err(ConnectionError::IOError(e)) }
+                    }
+                };
 
-                    // No decompression needed
-                }
+                compressed_stream
             }
-        }
+            // If no compression is set zero copy the packet stream
+            None => {
+                pk_stream
+            }
+        };
 
         // Encrypt the compressed data
-        if let Some(encryption) = &self.encryption {
-            todo!("Encrypt the data (after compression)")
-        };
-
-        // Write final data into buf
-        match buf.write_all(&*buf_pks) {
-            Ok(_) => {}
-            Err(_) => return Err(ConnectionError::WriteIOError),
+        let encrypted_stream = match &self.encryption {
+            Some(encryption) => {
+                todo!("Encrypt the data (after compression)")
+            }
+            None => {
+                compressed_stream
+            }
         };
 
         // Send the data
-        // TODO: figure out if immediate: true should be used
-        match self.rak_connection.send(&*buf, true).await {
+        match self.connection.send(&encrypted_stream.freeze()).await {
             Ok(_) => {}
-            Err(_) => return Err(ConnectionError::RakNetError),
+            Err(e) => return Err(ConnectionError::TransportError(e)),
         }
 
         Ok(())
     }
 
     pub async fn recv_gamepackets(&mut self) -> Result<Vec<GamePacket>, ConnectionError> {
-        // Recvieve data and turn it into cursor
-        let mut data = match self.rak_connection.recv().await {
-            Ok(v) => Cursor::new(v),
-            Err(_) => return Err(ConnectionError::ConnectionClosed),
+        // Receive data and turn it into cursor
+        let mut stream = match self.connection.recv().await {
+            Ok(v) => v,
+            Err(e) => return Err(ConnectionError::TransportError(e)),
         };
 
-        // Read Raknet Header
-        match data.read_u8() {
-            Ok(v) => {
-                match v {
-                    RAKNET_GAME_PACKET_ID => {}
-                    // Invalid Raknet packet header
-                    _ => return Err(ConnectionError::InvalidRakNetHeader),
-                }
+        let mut decrypted_stream = match &self.encryption {
+            Some(encryption) => {
+                todo!("Decrypt the data (before decompression)")
             }
-            Err(_) => return Err(ConnectionError::ReadIOError),
+            None => { stream }
         };
 
-        // TODO: Decrypt the data (before decompression)
+        // Decompress data
+        let mut decompressed_stream = match &self.compression {
+            Some(compression) => {
+                match decrypted_stream.read_u8() {
+                    Ok(v) => { if v != compression.id_u8() {
 
-        // Decompress data depending on compression method
-        if let Some(compression) = &self.compression {
-            // Read compression algorythm
-            let compression_id = match data.read_u8() {
-                Ok(v) => v,
-                Err(_) => return Err(ConnectionError::ReadIOError),
-            };
+                    }}
+                    Err(_) => {}
+                };
 
-            let pos = data.position() as usize;
+                let mut decompressed_stream = ByteStreamWrite::new();
 
-            match compression_id {
-                ZlibCompression::ID_u8 => match compression {
-                    CompressionMethods::Zlib(zlib) => {
-                        data =
-                            Cursor::new(match zlib.decompress(data.into_inner()[pos..].to_vec()) {
-                                Ok(v) => v,
-                                Err(e) => return Err(ConnectionError::CompressError(e)),
-                            })
-                    }
-                    CompressionMethods::Snappy(_) => {
-                        return Err(ConnectionError::WrongCompressionMethod);
-                    }
-                    CompressionMethods::None => {
-                        return Err(ConnectionError::WrongCompressionMethod);
-                    }
-                },
-                SnappyCompression::ID_u8 => match compression {
-                    CompressionMethods::Zlib(_) => {
-                        return Err(ConnectionError::WrongCompressionMethod);
-                    }
-                    CompressionMethods::Snappy(snappy) => {
-                        data = Cursor::new(
-                            match snappy.decompress(data.into_inner()[pos..].to_vec()) {
-                                Ok(v) => v,
-                                Err(e) => return Err(ConnectionError::CompressError(e)),
-                            },
-                        )
-                    }
-                    CompressionMethods::None => {
-                        return Err(ConnectionError::WrongCompressionMethod);
-                    }
-                },
-                NoCompression::ID_u8 => {}
-                other => return Err(ConnectionError::UnknownCompressionMethod(other)),
+                match compression.decompress(&mut decrypted_stream, &mut decompressed_stream) {
+                    Ok(_) => {}
+                    Err(e) => { return Err(ConnectionError::CompressError(e)) }
+                };
+
+                decompressed_stream.freeze()
             }
-        }
+            None => { decrypted_stream }
+        };
 
         let mut gamepackets = vec![];
 
         // Read gamepacket loop
         'gamepacket_read: loop {
             // Deserialize gamepacket
-            match GamePacket::pk_deserialize(&mut data) {
+            match GamePacket::pk_deserialize(&mut decompressed_stream) {
                 Ok(v) => gamepackets.push(v.0),
                 Err(e) => return Err(ConnectionError::ProtoCodecError(e)),
             };
 
-            // Is at end of batched packet data cursor
-            if data.position() == data.get_ref().len() as u64 {
+            // Is at the end of batched packet data cursor
+            if decompressed_stream.position() == decompressed_stream.len() as u64 {
                 break 'gamepacket_read;
             }
         }
