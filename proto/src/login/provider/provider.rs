26ebd7a4305cad12a6609ffd472549ccf8992bd8
Adrian <adrian.olbrich1@gmail.com>
2024-06-13 10:08:29 +0200

    make Compression better

--- proto/src/login/provider/provider.rs
+++ proto/src/login/provider/provider.rs
@@ -5,7 +5,7 @@ use crate::packets::network_settings::NetworkSettingsPacket;
 use crate::packets::network_settings_request::NetworkSettingsRequestPacket;
 
 pub trait LoginProviderServer {
-    fn compression(&self) -> &Compression;
+    fn compression(&self) -> Compression;
     fn encryption_enabled(&self) -> bool;
 
     fn on_network_settings_request_pk(&self, pk: &mut NetworkSettingsRequestPacket) -> LoginProviderStatus{
