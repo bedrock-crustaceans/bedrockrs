55135803f131cdfe351a916dfaf3d78c83ca7d67
Adrian <adrian.olbrich1@gmail.com>
2024-06-13 10:13:03 +0200

    add default login impl provider

--- proto/src/login/provider/default.rs
+++ proto/src/login/provider/default.rs
@@ -1,5 +1,8 @@
 use crate::compression::Compression;
-use crate::login::provider::LoginProviderServer;
+use crate::login::provider::{LoginProviderServer, LoginProviderStatus};
+use crate::packets::login::LoginPacket;
+use crate::packets::network_settings::NetworkSettingsPacket;
+use crate::packets::network_settings_request::NetworkSettingsRequestPacket;
 
 pub struct DefaultLoginProvider {
 
@@ -12,11 +15,26 @@ impl DefaultLoginProvider {
 }
 
 impl LoginProviderServer for DefaultLoginProvider {
-    fn compression(&self) -> &Compression {
-        &Compression::None
+    fn compression(&self) -> Compression {
+        Compression::None
     }
 
     fn encryption_enabled(&self) -> bool {
         false
+    }
+
+    fn on_network_settings_request_pk(&self, pk: &mut NetworkSettingsRequestPacket) -> LoginProviderStatus {
+        println!("{:#?}", pk);
+        LoginProviderStatus::ContinueLogin
+    }
+
+    fn on_network_settings_pk(&self, pk: &mut NetworkSettingsPacket) -> LoginProviderStatus {
+        println!("{:#?}", pk);
+        LoginProviderStatus::ContinueLogin
+    }
+
+    fn on_login_pk(&self, pk: &mut LoginPacket) -> LoginProviderStatus {
+        println!("{:#?}", pk);
+        LoginProviderStatus::ContinueLogin
     }
 }
