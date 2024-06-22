87c7950904d01aac0bb5a27c049600c8535d00ec
Adrian <adrian.olbrich1@gmail.com>
2024-06-22 21:11:39 +0200

    add PackURL

--- proto/src/login/provider/packs.rs
+++ proto/src/login/provider/packs.rs
@@ -1,10 +1,12 @@
 use packs::behavior::behavior_pack::BehaviorPack;
 use packs::resource::resource_pack::ResourcePack;
+use crate::types::pack_url::PackURL;
 
 pub enum LoginProviderPacks {
     CDN {
         behavior_packs: Vec<BehaviorPack>,
         resource_packs: Vec<ResourcePack>,
+        cdn_urls: Vec<PackURL>,
     },
     DirectNetworkTransfer {
         behavior_packs: Vec<BehaviorPack>,
