ebed5bc35d596e99c8243b2dc2bba80854c7130a
adrian <adrian.olbrich1@gmail.com>
2024-06-30 17:07:04 +0200

    rewrite debug impl

--- bedrock_core/src/version.rs
+++ bedrock_core/src/version.rs
@@ -1,11 +1,17 @@
 use std::fmt::{Debug, Display, Formatter};
 
-#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub struct Version {
     major: u32,
     minor: u32,
     patch: u32,
     build: u32,
+}
+
+impl Debug for Version {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}.{}.{}.{}", self.major, self.minor, self.patch, self.build)
+    }
 }
 
 impl Display for Version {
