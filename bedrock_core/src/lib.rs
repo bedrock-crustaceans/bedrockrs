46a29e2fd195984ba9469fb3742ac4f7b3518ccd
Adrian <adrian.olbrich1@gmail.com>
2024-07-03 10:01:31 +0200

    reexports and public

--- bedrock_core/src/lib.rs
+++ bedrock_core/src/lib.rs
@@ -10,6 +10,7 @@ pub use vec::vec2::Vec2;
 pub use vec::vec2f::Vec2f;
 pub use vec::vec3::Vec3;
 pub use vec::vec3f::Vec3f;
+pub use semver::*;
 
 pub mod int;
 pub mod vec;
@@ -19,4 +20,4 @@ pub mod stream;
 pub mod difficulty;
 pub mod dimension;
 pub mod permissions_level;
-mod semver;
+pub mod semver;
