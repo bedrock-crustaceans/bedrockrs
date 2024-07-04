0c25d28d788222dc99cc0dee73edef057a96788c
Adrian <adrian.olbrich1@gmail.com>
2024-07-04 15:54:48 +0200

    make all public

--- proto/src/types/network_block_pos.rs
+++ proto/src/types/network_block_pos.rs
@@ -3,7 +3,7 @@ use proto_derive::ProtoCodec;
 
 #[derive(ProtoCodec, Debug, Clone)]
 pub struct NetworkBlockPos {
-    x: VAR<i32>,
-    y: VAR<u32>,
-    z: VAR<i32>,
+    pub x: VAR<i32>,
+    pub y: VAR<u32>,
+    pub z: VAR<i32>,
 }
