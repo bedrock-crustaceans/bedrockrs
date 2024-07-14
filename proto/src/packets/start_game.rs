85edb777b4ba6923aaa7e4b77cf4ae83b6d15ea1
theaddon <theaddonn@gmail.com>
2024-07-14 21:37:56 +0200

    use float vecs

--- proto/src/packets/start_game.rs
+++ proto/src/packets/start_game.rs
@@ -1,5 +1,5 @@
 use bedrock_core::gamemode::Gamemode;
-use bedrock_core::{ActorRuntimeID, ActorUniqueID, Uuid, Vec2, Vec3, LE, VAR};
+use bedrock_core::{ActorRuntimeID, ActorUniqueID, Uuid, Vec2, Vec3, LE, VAR, Vec2f, Vec3f};
 use nbt::NbtTag;
 use proto_derive::ProtoCodec;
 
@@ -12,8 +12,8 @@ pub struct StartGamePacket {
     pub target_actor_id: ActorUniqueID,
     pub target_runtime_id: ActorRuntimeID,
     pub actor_game_type: Gamemode,
-    pub position: Vec3,
-    pub rotation: Vec2,
+    pub position: Vec3f,
+    pub rotation: Vec2f,
     pub settings: LevelSettings,
     pub level_id: String,
     pub level_name: String,
@@ -23,10 +23,10 @@ pub struct StartGamePacket {
     pub current_level_time: LE<u64>,
     pub enchantment_seed: VAR<i32>,
     // TODO Add real value
-    #[len_type(VAR::< u32 >)]
+    #[len_type(VAR::<u32>)]
     pub block_properties: Vec<u8>,
     // TODO Add real value
-    #[len_type(VAR::< u32 >)]
+    #[len_type(VAR::<u32>)]
     pub items: Vec<u8>,
     pub multiplayer_correlation_id: String,
     pub enable_item_stack_net_manager: bool,
