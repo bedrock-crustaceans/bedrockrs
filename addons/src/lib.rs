db9e90d5f07c8e9c0af5129ab95d4e5b61d1bbc9
theaddon <theaddonn@gmail.com>
2024-07-25 02:58:37 +0200

    use custom debug impl for AddonDynamicVersion

--- addons/src/lib.rs
+++ addons/src/lib.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Debug, Formatter};
 use std::path::Path;
 
 use bedrockrs_core::Vec3;
@@ -25,8 +26,20 @@ pub trait Addon {
 }
 
 /// A version used in Addons that is either a Vector [a, b, c] or SemVer String.
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(untagged)]
 pub enum AddonDynamicVersion {
     Vector(Vec3<u32>),
     SemVer(semver::Version),
+}
+
+impl Debug for AddonDynamicVersion {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AddonDynamicVersion::Vector(v) => {
+                f.debug_list().entries([v.x, v.y, v.z]).finish()
+            }
+            AddonDynamicVersion::SemVer(v) => { v.fmt(f) }
+        }
+    }
 }
