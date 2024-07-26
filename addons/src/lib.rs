eb44a4bdc6ae44e390d9d059fa0feecf8ea4c8da
theaddon <theaddonn@gmail.com>
2024-07-26 18:38:02 +0200

    move version into own module

--- addons/src/lib.rs
+++ addons/src/lib.rs
@@ -12,6 +12,8 @@ pub mod error;
 pub mod language;
 pub mod manifest;
 pub mod resource;
+mod version;
+mod identifier;
 
 pub trait Addon {
     fn manifest(&self) -> &AddonManifest;
@@ -25,19 +27,3 @@ pub trait Addon {
         Self: Sized;
 }
 
-/// A version used in Addons that is either a Vector [a, b, c] or SemVer String.
-#[derive(Serialize, Deserialize, Clone)]
-#[serde(untagged)]
-pub enum AddonDynamicVersion {
-    Vector(Vec3<u32>),
-    SemVer(semver::Version),
-}
-
-impl Debug for AddonDynamicVersion {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            AddonDynamicVersion::Vector(v) => f.debug_list().entries([v.x, v.y, v.z]).finish(),
-            AddonDynamicVersion::SemVer(v) => v.fmt(f),
-        }
-    }
-}
