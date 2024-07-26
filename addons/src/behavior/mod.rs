b9d75d8e62e4056231297a31c068347e339e79c0
theaddon <theaddonn@gmail.com>
2024-07-26 18:37:14 +0200

    reimpl behavior pack

--- addons/src/behavior/mod.rs
+++ addons/src/behavior/mod.rs
@@ -1,6 +1,15 @@
+mod blocks;
+
+use std::collections::HashMap;
+use std::path::PathBuf;
+use crate::behavior::blocks::AddonBlock;
+use crate::language::Languages;
 use crate::manifest::AddonManifest;
 
 #[derive(Debug, Clone)]
 pub struct BehaviorPack {
     manifest: AddonManifest,
+    languages: Languages,
+    blocks: HashMap<PathBuf, AddonBlock>,
+    functions: HashMap<PathBuf, String>
 }
