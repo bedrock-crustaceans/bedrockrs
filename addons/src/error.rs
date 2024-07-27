797196dc9293e1b3efe13150e15be98eee2f50b9
theaddon <theaddonn@gmail.com>
2024-07-27 22:54:41 +0200

    add needed error types

--- addons/src/error.rs
+++ addons/src/error.rs
@@ -1,4 +1,12 @@
+use std::io;
+use std::path::PathBuf;
+use std::sync::Arc;
 use thiserror::Error;
 
 #[derive(Error, Debug, Clone)]
-pub enum AddonError {}
+pub enum AddonError {
+    #[error("IOError at {0}: {1}")]
+    IOError(Arc<io::Error>, PathBuf),
+    #[error("JsonError at {0}: {1}")]
+    JsonError(Arc<serde_json::Error>, PathBuf),
+}
