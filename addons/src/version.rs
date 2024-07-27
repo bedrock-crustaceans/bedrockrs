fd1eefcb80d541318902206a964c820f0f1b3b7b
theaddon <theaddonn@gmail.com>
2024-07-27 22:53:57 +0200

    rename AddonDynamicVersion to AddonSemanticVersion

--- addons/src/version.rs
+++ addons/src/version.rs
@@ -6,16 +6,16 @@ use serde::{Deserialize, Serialize};
 /// A version used in Addons that is either a Vector [a, b, c] or SemVer String.
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(untagged)]
-pub enum AddonDynamicVersion {
+pub enum AddonSemanticVersion {
     Vector(Vec3<u32>),
     SemVer(semver::Version),
 }
 
-impl Debug for AddonDynamicVersion {
+impl Debug for AddonSemanticVersion {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            AddonDynamicVersion::Vector(v) => f.debug_list().entries([v.x, v.y, v.z]).finish(),
-            AddonDynamicVersion::SemVer(v) => v.fmt(f),
+            AddonSemanticVersion::Vector(v) => f.debug_list().entries([v.x, v.y, v.z]).finish(),
+            AddonSemanticVersion::SemVer(v) => v.fmt(f),
         }
     }
 }
