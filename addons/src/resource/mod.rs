3653a6541259c97134c1d5deb0525d3c89b57b45
theaddon <theaddonn@gmail.com>
2024-07-28 11:40:06 +0200

    add manifest and langauge parsing to rp

--- addons/src/resource/mod.rs
+++ addons/src/resource/mod.rs
@@ -1,9 +1,11 @@
+use std::fs;
 use std::path::Path;
-
+use std::sync::Arc;
 use crate::error::AddonError;
 use crate::language::Languages;
 use crate::manifest::AddonManifest;
 use crate::Addon;
+use crate::error::AddonError::{IOError, JsonError};
 
 #[derive(Debug, Clone)]
 pub struct ResourcePack {
@@ -20,7 +22,20 @@ impl Addon for ResourcePack {
     where
         Self: Sized,
     {
-        todo!()
+        let path = path.as_ref().to_path_buf();
+
+        // Manifest
+        let manifest_path = path.join("manifest.json");
+        let manifest = fs::read_to_string(&manifest_path).map_err(|e| IOError(Arc::new(e), manifest_path.clone()))?;
+        let manifest: AddonManifest = serde_json::from_str(&manifest).map_err(|e| JsonError(Arc::new(e), manifest_path))?;
+
+        // Languages
+        let languages = Languages::import(path.join("texts"))?;
+
+        Ok(Self {
+            manifest,
+            languages
+        })
     }
 
     fn export(path: impl AsRef<Path>) -> Result<Self, AddonError>
