9081b8a44108b4dbc03d48b38867e894afb7166d
theaddon <theaddonn@gmail.com>
2024-07-28 11:39:34 +0200

    make directories optional

--- addons/src/behavior/mod.rs
+++ addons/src/behavior/mod.rs
@@ -21,7 +21,7 @@ mod menu_category;
 #[derive(Debug, Clone)]
 pub struct BehaviorPack {
     manifest: AddonManifest,
-    //languages: Languages,
+    languages: Languages,
     blocks: HashMap<PathBuf, AddonBlock>,
     items: HashMap<PathBuf, AddonItem>,
     functions: HashMap<PathBuf, String>,
@@ -45,35 +45,37 @@ impl Addon for BehaviorPack {
         let manifest: AddonManifest = serde_json::from_str(&manifest).map_err(|e| JsonError(Arc::new(e), manifest_path))?;
 
         // Languages
-        // TODO: Impl langauge parsing
-        //let languages = Languages::import(path.join("blocks"))?;
+        let languages = Languages::import(path.join("texts"))?;
 
         // Blocks
         let blocks_path = path.join("blocks");
         let mut blocks = HashMap::new();
 
-        'blocks_walk: for blocks_entry in WalkDir::new(&blocks_path)
-            .into_iter()
-            .filter(|v| { if let Ok(v) = v { v.file_type().is_file() } else { false } }) {
+        // If dir exists read all blocks
+        if blocks_path.is_dir() {
+            'blocks_walk: for blocks_entry in WalkDir::new(&blocks_path)
+                .into_iter()
+                .filter(|v| { if let Ok(v) = v { v.file_type().is_file() } else { false } }) {
 
-            if let Ok(entry) = blocks_entry {
-                let block_path = entry.path();
+                if let Ok(entry) = blocks_entry {
+                    let block_path = entry.path();
 
-                let block = fs::read_to_string(block_path)
-                    .map_err(|e| IOError(Arc::new(e), block_path.to_path_buf()))?;
+                    let block = fs::read_to_string(block_path)
+                        .map_err(|e| IOError(Arc::new(e), block_path.to_path_buf()))?;
 
-                // If the file is empty skip it
-                if block.trim().is_empty() {
-                    continue 'blocks_walk;
+                    // If the file is empty skip it
+                    if block.trim().is_empty() {
+                        continue 'blocks_walk;
+                    }
+
+                    // Strip all c-style comments
+                    let stripper = CommentSettings::c_style().strip_comments(block.as_bytes());
+
+                    let block: AddonBlock = serde_json::from_reader(stripper)
+                        .map_err(|e| JsonError(Arc::new(e), block_path.to_path_buf()))?;
+
+                    blocks.insert(block_path.to_path_buf(), block);
                 }
-
-                // Strip all c-style comments
-                let stripper = CommentSettings::c_style().strip_comments(block.as_bytes());
-
-                let block: AddonBlock = serde_json::from_reader(stripper)
-                    .map_err(|e| JsonError(Arc::new(e), block_path.to_path_buf()))?;
-
-                blocks.insert(block_path.to_path_buf(), block);
             }
         }
 
@@ -81,28 +83,31 @@ impl Addon for BehaviorPack {
         let items_path = path.join("items");
         let mut items = HashMap::new();
 
-        'items_walk: for items_entry in WalkDir::new(&items_path)
-            .into_iter()
-            .filter(|v| { if let Ok(v) = v { v.file_type().is_file() } else { false } }) {
+        // If dir exists read all items
+        if items_path.is_dir() {
+            'items_walk: for items_entry in WalkDir::new(&items_path)
+                .into_iter()
+                .filter(|v| { if let Ok(v) = v { v.file_type().is_file() } else { false } }) {
 
-            if let Ok(entry) = items_entry {
-                let item_path = entry.path();
+                if let Ok(entry) = items_entry {
+                    let item_path = entry.path();
 
-                let item = fs::read_to_string(item_path)
-                    .map_err(|e| IOError(Arc::new(e), item_path.to_path_buf()))?;
+                    let item = fs::read_to_string(item_path)
+                        .map_err(|e| IOError(Arc::new(e), item_path.to_path_buf()))?;
 
-                // If the file is empty skip it
-                if item.trim().is_empty() {
-                    continue 'items_walk;
+                    // If the file is empty skip it
+                    if item.trim().is_empty() {
+                        continue 'items_walk;
+                    }
+
+                    // Strip all c-style comments
+                    let stripper = CommentSettings::c_style().strip_comments(item.as_bytes());
+
+                    let item: AddonItem = serde_json::from_reader(stripper)
+                        .map_err(|e| JsonError(Arc::new(e), item_path.to_path_buf()))?;
+
+                    items.insert(item_path.to_path_buf(), item);
                 }
-
-                // Strip all c-style comments
-                let stripper = CommentSettings::c_style().strip_comments(item.as_bytes());
-
-                let item: AddonItem = serde_json::from_reader(stripper)
-                    .map_err(|e| JsonError(Arc::new(e), item_path.to_path_buf()))?;
-
-                items.insert(item_path.to_path_buf(), item);
             }
         }
 
@@ -110,41 +115,47 @@ impl Addon for BehaviorPack {
         let functions_path = path.join("functions");
         let mut functions = HashMap::new();
 
-        for functions_entry in WalkDir::new(&functions_path)
-            .into_iter()
-            .filter(|v| { if let Ok(v) = v { v.file_type().is_file() } else { false } }) {
+        // If dir exists read all functions
+        if functions_path.is_dir() {
+            for functions_entry in WalkDir::new(&functions_path)
+                .into_iter()
+                .filter(|v| { if let Ok(v) = v { v.file_type().is_file() } else { false } }) {
 
-            if let Ok(entry) = functions_entry {
-                let function_path = entry.path();
+                if let Ok(entry) = functions_entry {
+                    let function_path = entry.path();
 
-                let function = fs::read_to_string(function_path)
-                    .map_err(|e| IOError(Arc::new(e), function_path.to_path_buf()))?;
+                    let function = fs::read_to_string(function_path)
+                        .map_err(|e| IOError(Arc::new(e), function_path.to_path_buf()))?;
 
-                functions.insert(function_path.to_path_buf(), function);
+                    functions.insert(function_path.to_path_buf(), function);
+                }
             }
         }
 
-        // Functions
+        // Scripts
         let scripts_path = path.join("scripts");
         let mut scripts = HashMap::new();
 
-        for scripts_entry in WalkDir::new(&scripts_path)
-            .into_iter()
-            .filter(|v| { if let Ok(v) = v { v.file_type().is_file() } else { false } }) {
+        // If dir exists read all scripts
+        if scripts_path.is_dir() {
+            for scripts_entry in WalkDir::new(&scripts_path)
+                .into_iter()
+                .filter(|v| { if let Ok(v) = v { v.file_type().is_file() } else { false } }) {
 
-            if let Ok(entry) = scripts_entry {
-                let script_path = entry.path();
+                if let Ok(entry) = scripts_entry {
+                    let script_path = entry.path();
 
-                let script = fs::read_to_string(script_path)
-                    .map_err(|e| IOError(Arc::new(e), script_path.to_path_buf()))?;
+                    let script = fs::read_to_string(script_path)
+                        .map_err(|e| IOError(Arc::new(e), script_path.to_path_buf()))?;
 
-                scripts.insert(script_path.to_path_buf(), script);
+                    scripts.insert(script_path.to_path_buf(), script);
+                }
             }
         }
 
         Ok(Self {
             manifest,
-            //languages,
+            languages,
             blocks,
             items,
             functions,
