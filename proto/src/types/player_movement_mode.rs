dd4cdf1e9a8741dea7beac459e62dbaeb14a38d8
theaddon <theaddonn@gmail.com>
2024-07-30 13:39:06 +0200

    use protocodec amcro for player_movement_mode

--- proto/src/types/player_movement_mode.rs
+++ proto/src/types/player_movement_mode.rs
@@ -1,29 +1,11 @@
-use std::io::Cursor;
-use std::sync::Arc;
-
 use bedrockrs_core::int::VAR;
-use bedrockrs_proto_core::error::ProtoCodecError;
-use bedrockrs_proto_core::ProtoCodec;
+use bedrockrs_proto_derive::ProtoCodec;
 
-#[derive(Debug, Clone)]
+#[derive(ProtoCodec, Debug, Clone)]
+#[enum_repr(VAR::<i32>)]
 pub enum PlayerMovementMode {
-    Client,
-    Server,
-    ServerWithRewind,
+    Client = 0,
+    Server = 1,
+    ServerWithRewind = 2,
 }
 
-impl ProtoCodec for PlayerMovementMode {
-    fn proto_serialize(&self, stream: &mut Vec<u8>) -> Result<(), ProtoCodecError> {
-        VAR::<i32>::new(match self {
-            PlayerMovementMode::Client => 0x00,
-            PlayerMovementMode::Server => 0x01,
-            PlayerMovementMode::ServerWithRewind => 0x02,
-        })
-        .write(stream)
-        .map_err(|e| ProtoCodecError::IOError(Arc::new(e)))
-    }
-
-    fn proto_deserialize(stream: &mut Cursor<&[u8]>) -> Result<Self, ProtoCodecError> {
-        todo!()
-    }
-}
