129ecbc1ab06151954db1847b9922fa5207f7101
theaddon <theaddonn@gmail.com>
2024-07-30 13:40:20 +0200

    use protocodec amcro for spawn_biome_type

--- proto/src/types/spawn_biome_type.rs
+++ proto/src/types/spawn_biome_type.rs
@@ -4,26 +4,11 @@ use std::sync::Arc;
 use bedrockrs_core::int::LE;
 use bedrockrs_proto_core::error::ProtoCodecError;
 use bedrockrs_proto_core::ProtoCodec;
+use bedrockrs_proto_derive::ProtoCodec;
 
-#[derive(Debug, Clone)]
+#[derive(ProtoCodec, Debug, Clone)]
+#[repr(LE::<i16>)]
 pub enum SpawnBiomeType {
-    Default,
-    UserDefined,
-}
-
-impl ProtoCodec for SpawnBiomeType {
-    fn proto_serialize(&self, stream: &mut Vec<u8>) -> Result<(), ProtoCodecError> {
-        let int = match self {
-            SpawnBiomeType::Default => 0x00,
-            SpawnBiomeType::UserDefined => 0x01,
-        };
-
-        LE::<i16>::new(int)
-            .write(stream)
-            .map_err(|e| ProtoCodecError::IOError(Arc::new(e)))
-    }
-
-    fn proto_deserialize(stream: &mut Cursor<&[u8]>) -> Result<Self, ProtoCodecError> {
-        todo!()
-    }
+    Default = 0,
+    UserDefined = 1,
 }
