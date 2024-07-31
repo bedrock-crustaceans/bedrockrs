fac62ff0bacde9c75f95e029a390470a4d1d6ec6
theaddon <theaddonn@gmail.com>
2024-08-01 01:13:35 +0200

    add missing enum variants

--- proto/src/types/play_status.rs
+++ proto/src/types/play_status.rs
@@ -7,17 +7,19 @@ pub enum PlayStatusType {
     /// Sent after Login has been successfully decoded and the player has logged in
     LoginSuccess = 0,
     /// Displays "Could not connect: Outdated client!"
-    FailedClient = 1,
+    FailedClientOld = 1,
     /// Displays "Could not connect: Outdated server!"
-    FailedServer = 2,
+    FailedServerOld = 2,
     /// Sent after world data to spawn the player
     PlayerSpawn = 3,
     /// Displays "Unable to connect to world. Your school does not have access to this server."
     FailedInvalidTenant = 4,
     /// Displays "The server is not running Minecraft: Education Edition. Failed to connect."
-    FailedVanillaEdu = 5,
+    FailedEditionMismatchEduToVanilla = 5,
     /// Displays "The server is running an incompatible edition of Minecraft. Failed to connect."
-    FailedIncompatible = 6,
+    FailedEditionMismatchVanillaToEdu = 6,
     /// Displays "Wow this server is popular! Check back later to see if space opens up. Server Full"
     FailedServerFull = 7,
+    FailedEditorMismatchEditorToVanilla = 8,
+    FailedEditorMismatchVanillaToEditor = 9,
 }
