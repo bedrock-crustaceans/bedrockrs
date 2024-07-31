9a53675ac704f95f7a78a87285e222cf37bd61ab
theaddon <theaddonn@gmail.com>
2024-07-31 23:07:06 +0200

    add forgotten imports

--- proto/src/packets/emote_list.rs
+++ proto/src/packets/emote_list.rs
@@ -1,6 +1,7 @@
 use bedrockrs_proto_derive::ProtoCodec;
 use bedrockrs_shared::actor_runtime_id::ActorRuntimeID;
 use uuid::Uuid;
+use bedrockrs_core::int::VAR;
 
 #[derive(ProtoCodec, Debug, Clone)]
 pub struct EmoteListPacket {
