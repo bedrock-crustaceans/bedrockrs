3160ffb667d48423e0991b4e84f2f738ae36d80c
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2024-08-02 16:49:32 +0100

    push b4 moving on

--- proto/src/packets/player_action.rs
+++ proto/src/packets/player_action.rs
@@ -3,6 +3,48 @@ use bedrockrs_proto_derive::ProtoCodec;
 use crate::types::network_block_pos::NetworkBlockPos;
 use bedrockrs_shared::actor_runtime_id::ActorRuntimeID;
 
+pub enum PlayerActionType {
+    Unknown = -1,
+    StartDestroyBlock = 0,
+    AbortDestroyBlock,
+    StopDestroyBlock,
+    GetUpdatedBlock,
+    DropItem,
+    StartSleeping,
+    StopSleeping,
+    Respawn,
+    StartJump,
+    StartSprinting,
+    StopSprinting,
+    StartSneaking,
+    StopSneaking,
+    CreativeDestroyBlock,
+    ChangeDimensionAck,
+    StartGliding,
+    StopGlibiding,
+    DenyDestroyBlock,
+    CrackBlock,
+    ChangeSkin,
+    DeprecatedUpdatedEnchantingSeed,
+    StartSwimming,
+    StopSwimming,
+    StartSpinAttack,
+    StopSpinAttack,
+    InteractWithBlock,
+    PredictDestroyBlock,
+    ContinueDestroyBlock,
+    StartItemUseOn,
+    StopItemUseOn,
+    HandledTeleport,
+    MissedSwing,
+    StartCrawling,
+    StopCrawling,
+    StartFlying,
+    StopFlying,
+    ClientAckServerData,
+    Count
+}
+
 #[derive(ProtoCodec, Debug, Clone)]
 pub struct PlayerActionPacket {
     pub player_runtime_id: ActorRuntimeID,
