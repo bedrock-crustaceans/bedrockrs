30122a0c70e9e099117116572271cb4bd10a0ec4
banchen19 <banchen19@outlook.com>
2024-08-04 06:25:26 +0800

    更新 player_action.rs

--- crates/proto/src/packets/player_action.rs
+++ crates/proto/src/packets/player_action.rs
@@ -1,8 +1,8 @@
 use crate::types::network_block_pos::NetworkBlockPos;
 use bedrockrs_core::int::VAR;
 use bedrockrs_shared::actor_runtime_id::ActorRuntimeID;
-
-#[derive(Debug, Clone)]
+use bedrockrs_proto_core::ProtoCodec;
+#[derive(ProtoCodec,Debug, Clone)]
 pub struct PlayerActionPacket {
     pub player_runtime_id: ActorRuntimeID,
     pub action: VAR<i32>,
@@ -10,24 +10,4 @@ pub struct PlayerActionPacket {
     pub result_pos: NetworkBlockPos,
     pub face: VAR<i32>,
 }
-use bedrockrs_proto_core::ProtoCodec;
-impl ProtoCodec for PlayerActionPacket {
-    fn proto_serialize(
-        &self,
-        stream: &mut Vec<u8>,
-    ) -> Result<(), bedrockrs_proto_core::error::ProtoCodecError> {
-        Ok(())
-    }
 
-    fn proto_deserialize(
-        stream: &mut std::io::Cursor<&[u8]>,
-    ) -> Result<Self, bedrockrs_proto_core::error::ProtoCodecError> {
-        Ok(Self {
-            player_runtime_id: ActorRuntimeID::proto_deserialize(stream)?,
-            action: VAR::<i32>::proto_deserialize(stream)?,
-            block_pos: NetworkBlockPos::proto_deserialize(stream)?,
-            result_pos: NetworkBlockPos::proto_deserialize(stream)?,
-            face: VAR::<i32>::proto_deserialize(stream)?,
-        })
-    }
-}
