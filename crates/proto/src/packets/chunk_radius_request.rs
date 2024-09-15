74d9f84ce9561223baa5ef766ce95ad5eb0f4a08
theaddon <theaddonn@gmail.com>
2024-09-15 16:36:33 +0200

    fix ChunkRadiusRequestPacket::chunk_radius

--- crates/proto/src/packets/chunk_radius_request.rs
+++ crates/proto/src/packets/chunk_radius_request.rs
@@ -4,6 +4,6 @@ use bedrockrs_proto_macros::{gamepacket, ProtoCodec};
 #[gamepacket(id = 69)]
 #[derive(ProtoCodec, Debug, Clone)]
 pub struct ChunkRadiusRequestPacket {
-    pub chunk_radius: VAR<u32>,
+    pub chunk_radius: VAR<i32>,
     pub chunk_radius_max: u8,
 }
