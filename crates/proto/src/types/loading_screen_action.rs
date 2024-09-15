4295763a2974f91b60a22997f40b7ac962b71254
theaddon <theaddonn@gmail.com>
2024-09-15 16:28:14 +0200

    add LoadingScreenPacket

--- crates/proto/src/types/loading_screen_action.rs
+++ crates/proto/src/types/loading_screen_action.rs
@@ -2,7 +2,7 @@ use bedrockrs_proto_macros::ProtoCodec;
 use bedrockrs_core::int::VAR;
 
 #[derive(ProtoCodec, Debug, Clone)]
-#[enum_repr(VAR::<u32>)]
+#[enum_repr(VAR::<i32>)]
 pub enum LoadingScreenAction {
     Unknown = 0,
     Start = 1,
