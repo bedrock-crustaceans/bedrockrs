66019bd40e47f6dd4daca9180f99983439a28b8f
theaddon <theaddonn@gmail.com>
2024-09-15 19:02:59 +0200

    radius is a varint

--- crates/proto/src/packets/chunk_publisher_update.rs
+++ crates/proto/src/packets/chunk_publisher_update.rs
@@ -8,7 +8,7 @@ use bedrockrs_proto_macros::{gamepacket, ProtoCodec};
 #[derive(ProtoCodec, Debug, Clone)]
 pub struct ChunkPublisherUpdatePacket {
     pub position: BlockPos,
-    pub radius: LE<u32>,
+    pub radius: VAR<u32>,
     #[len_repr(VAR::<u32>)]
     pub saved_chunks: Vec<ChunkPos>,
 }
