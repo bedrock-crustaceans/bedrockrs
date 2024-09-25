56942fb626f7e92fe21a1c6276aa0875386893ba
theaddon <theaddonn@gmail.com>
2024-09-25 17:12:31 +0200

    correct blockpos impl

--- crates/proto/src/types/block_pos.rs
+++ crates/proto/src/types/block_pos.rs
@@ -1,19 +1,30 @@
-use bedrockrs_core::int::VAR;
-use bedrockrs_proto_macros::ProtoCodec;
+use std::io::Cursor;
+use std::mem::transmute;
+use varint_rs::{VarintReader, VarintWriter};
+use bedrockrs_proto_core::error::ProtoCodecError;
+use bedrockrs_proto_core::ProtoCodec;
 
-#[derive(ProtoCodec, Debug, Clone)]
+#[derive(Debug, Clone)]
 pub struct BlockPos {
-    pub x: VAR<i32>,
-    pub y: VAR<u32>,
-    pub z: VAR<i32>,
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
 }
 
-impl BlockPos {
-    pub fn new(x: i32, y: u32, z: i32) -> BlockPos {
-        Self {
-            x: VAR::new(x),
-            y: VAR::new(y),
-            z: VAR::new(z),
-        }
+impl ProtoCodec for BlockPos {
+    fn proto_serialize(&self, buf: &mut Vec<u8>) -> Result<(), ProtoCodecError> {
+        buf.write_i32_varint(self.x)?;
+        unsafe { buf.write_u32_varint(transmute(self.x))? };
+        buf.write_i32_varint(self.x)?;
+
+        Ok(())
+    }
+
+    fn proto_deserialize(buf: &mut Cursor<&[u8]>) -> Result<Self, ProtoCodecError> {
+        Ok(Self {
+            x: buf.read_i32_varint()?,
+            y: unsafe { transmute(buf.read_u32_varint()?) },
+            z: buf.read_i32_varint()?,
+        })
     }
 }
