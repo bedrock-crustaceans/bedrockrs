d59aea3c4c90347b1fe6eff472d61070158e9559
theaddon <theaddonn@gmail.com>
2024-09-26 21:48:49 +0200

    cleanup the bool impl

--- crates/proto_core/src/types/bool.rs
+++ crates/proto_core/src/types/bool.rs
@@ -1,41 +1,26 @@
 use std::io::Cursor;
-use std::sync::Arc;
-
-use bedrockrs_core::int::LE;
+use byteorder::{ReadBytesExt, WriteBytesExt};
 
 use crate::error::ProtoCodecError;
 use crate::ProtoCodec;
 
 impl ProtoCodec for bool {
-    fn proto_serialize(&self, buf: &mut Vec<u8>) -> Result<(), ProtoCodecError>
+    fn proto_serialize(&self, stream: &mut Vec<u8>) -> Result<(), ProtoCodecError>
     where
         Self: Sized,
     {
         match self {
-            true => LE::<u8>::new(1)
-                .write(buf)
-                .map_err(|e| ProtoCodecError::IOError(Arc::new(e))),
-            false => LE::<u8>::new(0)
-                .write(buf)
-                .map_err(|e| ProtoCodecError::IOError(Arc::new(e))),
-        }
+            true => stream.write_u8(1)?,
+            false => stream.write_u8(0)?,
+        };
+        
+        Ok(())
     }
 
     fn proto_deserialize(stream: &mut Cursor<&[u8]>) -> Result<Self, ProtoCodecError>
     where
         Self: Sized,
     {
-        // a Bool is represented as a byte
-        Ok(
-            match LE::<u8>::read(stream)
-                .map_err(|e| ProtoCodecError::IOError(Arc::new(e)))?
-                .into_inner()
-            {
-                // 0 is counted as false
-                0 => false,
-                // Anything above 0 is true
-                _ => true,
-            },
-        )
+        Ok(!matches!(stream.read_u8()?, 0))
     }
 }
