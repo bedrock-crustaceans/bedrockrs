11cca0caa2c16d53262fef1b7f6ec84066c8c910
theaddon <theaddonn@gmail.com>
2024-09-26 10:39:27 +0200

    add option impl for le, be and var

--- crates/proto_core/src/types/option.rs
+++ crates/proto_core/src/types/option.rs
@@ -1,31 +1,40 @@
-use std::io::Cursor;
-
+use crate::byteorder::{ProtoCodecBE, ProtoCodecLE, ProtoCodecVAR};
 use crate::error::ProtoCodecError;
 use crate::ProtoCodec;
+use std::io::Cursor;
 
-impl<T: ProtoCodec> ProtoCodec for Option<T> {
-    fn proto_serialize(&self, buf: &mut Vec<u8>) -> Result<(), ProtoCodecError>
-    where
-        Self: Sized,
-    {
-        match self {
-            None => false.proto_serialize(buf)?,
-            Some(v) => {
-                true.proto_serialize(buf)?;
-                v.proto_serialize(buf)?;
+macro_rules! impl_proto_option {
+    ($name:ident) => {
+        impl<T: $name> $name for Option<T> {
+            fn proto_serialize(&self, buf: &mut Vec<u8>) -> Result<(), ProtoCodecError>
+            where
+                Self: Sized,
+            {
+                match self {
+                    Some(v) => {
+                        bool::proto_serialize(*true, buf)?;
+                        v.proto_serialize(buf)?;
+                    }
+                    None => bool::proto_serialize(*false, buf)?,
+                }
+
+                Ok(())
             }
-        };
 
-        Ok(())
-    }
-
-    fn proto_deserialize(stream: &mut Cursor<&[u8]>) -> Result<Self, ProtoCodecError>
-    where
-        Self: Sized,
-    {
-        Ok(match bool::proto_deserialize(stream)? {
-            false => None,
-            true => Some(T::proto_deserialize(stream)?),
-        })
-    }
+            fn proto_deserialize(stream: &mut Cursor<&[u8]>) -> Result<Self, ProtoCodecError>
+            where
+                Self: Sized,
+            {
+                Ok(match bool::proto_deserialize(stream)? {
+                    false => None,
+                    true => Some(T::proto_deserialize(stream)?),
+                })
+            }
+        }
+    };
 }
+
+impl_proto_option!(ProtoCodec);
+impl_proto_option!(ProtoCodecLE);
+impl_proto_option!(ProtoCodecBE);
+impl_proto_option!(ProtoCodecVAR);
