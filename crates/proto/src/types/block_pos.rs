e3fe5356ea37cc8078ac4b3986eb1b710e1d3c21
theaddon <theaddonn@gmail.com>
2024-09-28 20:10:50 +0200

    add comments to describe weird behaviour

--- crates/proto/src/types/block_pos.rs
+++ crates/proto/src/types/block_pos.rs
@@ -14,6 +14,7 @@ pub struct BlockPos {
 impl ProtoCodec for BlockPos {
     fn proto_serialize(&self, buf: &mut Vec<u8>) -> Result<(), ProtoCodecError> {
         buf.write_i32_varint(self.x)?;
+        // the y i32 height is serialized as an u32
         unsafe { buf.write_u32_varint(transmute(self.x))? };
         buf.write_i32_varint(self.x)?;
 
@@ -23,6 +24,7 @@ impl ProtoCodec for BlockPos {
     fn proto_deserialize(buf: &mut Cursor<&[u8]>) -> Result<Self, ProtoCodecError> {
         Ok(Self {
             x: buf.read_i32_varint()?,
+            // the y i32 height is deserialized as an u32
             y: unsafe { transmute(buf.read_u32_varint()?) },
             z: buf.read_i32_varint()?,
         })
