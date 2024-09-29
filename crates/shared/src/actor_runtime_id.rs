28d761a500fd7a0c31eb4bf97be739ce477b868c
theaddon <theaddonn@gmail.com>
2024-09-29 09:35:02 +0200

    cargo fmt

--- crates/shared/src/actor_runtime_id.rs
+++ crates/shared/src/actor_runtime_id.rs
@@ -1,7 +1,7 @@
-use std::io::Cursor;
-use varint_rs::{VarintReader, VarintWriter};
 use bedrockrs_proto_core::error::ProtoCodecError;
 use bedrockrs_proto_core::ProtoCodec;
+use std::io::Cursor;
+use varint_rs::{VarintReader, VarintWriter};
 
 #[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct ActorRuntimeID(pub u64);
