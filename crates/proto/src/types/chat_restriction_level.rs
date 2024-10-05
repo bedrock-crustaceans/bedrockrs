f6eed7fa9dc2211237724ed5b8a86064424f607f
theaddon <theaddonn@gmail.com>
2024-10-05 18:52:28 +0200

    dehexanise descriminators

--- crates/proto/src/types/chat_restriction_level.rs
+++ crates/proto/src/types/chat_restriction_level.rs
@@ -3,7 +3,7 @@ use bedrockrs_macros::ProtoCodec;
 #[derive(ProtoCodec, Debug, Clone)]
 #[enum_repr(i8)]
 pub enum ChatRestrictionLevel {
-    None = 0x00,
-    Dropped = 0x01,
-    Disabled = 0x02,
+    None = 0,
+    Dropped = 1,
+    Disabled = 2,
 }
