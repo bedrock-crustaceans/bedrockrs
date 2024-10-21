d74d317c163207ee01fbe5c857027e86bd74bb1e
theaddon <theaddonn@gmail.com>
2024-10-21 12:33:24 +0200

    fix uuid writing

--- crates/proto_core/src/types/uuid.rs
+++ crates/proto_core/src/types/uuid.rs
@@ -1,24 +1,23 @@
 use std::io::Cursor;
-
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use crate::error::ProtoCodecError;
 use crate::ProtoCodec;
 use uuid::Uuid;
-use varint_rs::{VarintReader, VarintWriter};
 
 impl ProtoCodec for Uuid {
     fn proto_serialize(&self, stream: &mut Vec<u8>) -> Result<(), ProtoCodecError> {
         let (upper, lower) = self.as_u64_pair();
 
-        stream.write_u64_varint(upper)?;
-        stream.write_u64_varint(lower)?;
+        stream.write_u64::<LittleEndian>(upper)?;
+        stream.write_u64::<LittleEndian>(lower)?;
 
         Ok(())
     }
 
     fn proto_deserialize(stream: &mut Cursor<&[u8]>) -> Result<Self, ProtoCodecError> {
         Ok(Uuid::from_u64_pair(
-            stream.read_u64_varint()?,
-            stream.read_u64_varint()?,
+            stream.read_u64::<LittleEndian>()?,
+            stream.read_u64::<LittleEndian>()?,
         ))
     }
 
