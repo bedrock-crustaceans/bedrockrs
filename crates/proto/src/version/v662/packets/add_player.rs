bc9418015310d6bea054877b35e1fb8fef166076
theaddon <theaddonn@gmail.com>
2024-11-03 13:04:17 +0100

    get compiling

--- crates/proto/src/version/v662/packets/add_player.rs
+++ crates/proto/src/version/v662/packets/add_player.rs
@@ -3,7 +3,7 @@ use bedrockrs_core::{Vec2, Vec3};
 use bedrockrs_macros::{gamepacket, ProtoCodec};
 use bedrockrs_shared::actor_runtime_id::ActorRuntimeID;
 use bedrockrs_shared::world::gamemode::Gamemode;
-use crate::version::v662::types::{ActorMetaData, ItemStackDescriptor};
+use crate::version::v662::types::{ItemStackDescriptor};
 
 #[gamepacket(id = 9)]
 #[derive(ProtoCodec, Debug, Clone)]
@@ -22,5 +22,5 @@ pub struct AddPlayerPacket {
     pub head_yaw: f32,
     pub carried_item: ItemStackDescriptor,
     pub gamemode: Gamemode,
-    pub actor_meta_data: ActorMetaData,
+    //pub actor_meta_data: ActorMetaData,
 }
