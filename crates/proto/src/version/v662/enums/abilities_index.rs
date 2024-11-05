3b792664aa1abcbb8471eb4a71f9bca1f176be3b
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-05 20:50:43 +1300

    format

--- crates/proto/src/version/v662/enums/abilities_index.rs
+++ crates/proto/src/version/v662/enums/abilities_index.rs
@@ -1,4 +1,3 @@
-use std::io::Cursor;
 use bedrockrs_macros::ProtoCodec;
 
 #[derive(ProtoCodec)]
--- crates/proto/src/version/v662/enums/actor_block_sync_message.rs
+++ crates/proto/src/version/v662/enums/actor_block_sync_message.rs
@@ -1,5 +1,6 @@
 pub mod ActorBlockSyncMessage {
     use bedrockrs_macros::ProtoCodec;
+    
     #[derive(ProtoCodec)]
     pub enum MessageId {
         NONE = 0,
