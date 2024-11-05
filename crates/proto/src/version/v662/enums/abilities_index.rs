44c6737690167d1d3f734a039de556bf1f7425c2
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-05 20:35:57 +1300

    [enum] abilities_index.rs

---
+++ crates/proto/src/version/v662/enums/abilities_index.rs
@@ -0,0 +1,27 @@
+use std::io::Cursor;
+use bedrockrs_macros::ProtoCodec;
+
+#[derive(ProtoCodec)]
+pub enum AbilitiesIndex {
+    Invalid = -1,
+    Build = 0,
+    Mine = 1,
+    DoorsAndSwitches = 2,
+    OpenContainers = 3,
+    AttackPlayers = 4,
+    AttackMobs = 5,
+    OperatorCommands = 6,
+    Teleport = 7,
+    Invulnerable = 8,
+    Flying = 9,
+    MayFly = 10,
+    Instabuild = 11,
+    Lightning = 12,
+    FlySpeed = 13,
+    WalkSpeed = 14,
+    Muted = 15,
+    WorldBuilder = 16,
+    NoClip = 17,
+    PrivilegedBuilder = 18,
+    AbilityCount = 19,
+}
