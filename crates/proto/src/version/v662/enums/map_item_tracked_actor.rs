b87558dec5102751aae6edc4f205e5b0052c53cf
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-22 01:22:37 +1300

    [enum] map_item_tracked_actor.rs (MOD)

---
+++ crates/proto/src/version/v662/enums/map_item_tracked_actor.rs
@@ -0,0 +1,11 @@
+pub mod MapItemTrackedActor {
+    use bedrockrs_macros::ProtoCodec;
+    
+    #[derive(ProtoCodec)]
+    pub enum Type {
+        Entity = 0,
+        BlockEntity = 1,
+        Other = 2,
+        COUNT = 3,
+    }
+}
