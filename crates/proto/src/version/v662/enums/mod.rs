ae75c6463ddc8eadac8e1506f32f40d0e01e341e
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-22 02:41:37 +1300

    [enum] mod.rs

---
+++ crates/proto/src/version/v662/enums/mod.rs
@@ -0,0 +1,130 @@
+macro_rules! export {
+    ($name:ident) => {
+        mod $name;
+        pub use $name::*;
+    };
+}
+
+export!(abilities_index);
+export!(actor_block_sync_message); // Mod
+export!(actor_damage_cause);
+export!(actor_data_ids);
+export!(actor_event);
+export!(actor_flags);
+export!(actor_link_type);
+export!(actor_type);
+export!(agent_action_type);
+export!(animate_packet); // Mod
+export!(animation_mode);
+export!(attribute_modifier_operation);
+export!(attribute_operands);
+export!(book_edit_action);
+export!(boss_event_update_type);
+export!(build_platform);
+export!(camera_preset); // Mod
+export!(camera_shake_action);
+export!(camera_shake_type);
+export!(chat_restriction_level);
+export!(client_play_mode);
+export!(clientbound_debug_renderer_packet); // Mod
+export!(clientbound_map_item_data_packet); // Mod
+export!(code_builder_storage_query_options); // Mod
+export!(command_block_mode);
+export!(command_origin_type);
+export!(command_output_type);
+export!(command_parameter_option);
+export!(command_permission_level);
+export!(complex_inventory_transaction); // Mod
+export!(connection); // Mod
+export!(container_enum_name);
+export!(container_id);
+export!(container_type);
+export!(crafting_data_entry_type);
+export!(crafting_type);
+export!(data_item_type);
+export!(difficulty);
+export!(easing_type);
+export!(editor); // Mod
+export!(education_edition_offer);
+export!(emote_packet); // Mod
+export!(enchant); // Mod
+export!(game_rule); // Mod
+export!(game_type);
+export!(generator_type);
+export!(hud_element);
+export!(hud_visibility);
+export!(identity_definition); // Mod
+export!(input_mode);
+export!(interact_packet); // Mod
+export!(inventory_layout);
+export!(inventory_left_tab_index);
+export!(inventory_right_tab_index);
+export!(inventory_source); // Mod
+export!(inventory_source_type); // TODO
+export!(item_descriptor); // Mod
+export!(item_release_inventory_transaction); // Mod
+export!(item_stack_net_result);
+export!(item_stack_request_action_type);
+export!(item_use_inventory_transaction); // Mod
+export!(item_use_method);
+export!(item_use_on_actor_inventory_transaction); // Mod
+export!(lab_table_packet); // Mod
+export!(lab_table_reaction_type);
+export!(legacy_telemetry_event_packet); // Mod
+export!(lesson_action);
+export!(level_event);
+export!(map_decoration); // Mod
+export!(map_item_tracked_actor); // Mod
+export!(minecraft_eventing); // Mod
+export!(minecraft_packet_ids);
+export!(mirror);
+export!(mob_effect_packet); // Mod
+export!(modal_form_cancel_reason);
+export!(molang_version);
+export!(multiplayer_settings_packet_type);
+export!(new_interaction_model);
+export!(npc_dialogue_packet); // Mod
+export!(npc_request_packet); // Mod
+export!(objective_sort_order);
+export!(pack_type);
+export!(packet_compression_algorithm);
+export!(packet_violation_severity);
+export!(packet_violation_type);
+export!(particle_type);
+export!(photo_type);
+export!(play_status);
+export!(player_action_type);
+export!(player_auth_input_packet); // Mod
+export!(player_list_packet_type);
+export!(player_permission_level);
+export!(player_position_mode_component); // Mod
+export!(player_respawn_state);
+export!(position_tracking_db_client_request_packet); // Mod
+export!(position_tracking_db_server_broadcast_packet); // Mod
+export!(puv); // Mod
+export!(request_ability_packet); // Mod
+export!(resource_pack_response);
+export!(rotation);
+export!(score_packet_type);
+export!(scoreboard_identity_packet_type);
+export!(serialized_abilities_data); // Mod
+export!(server_auth_movement_mode);
+export!(set_title_packet); // Mod
+export!(show_credits_packet); // Mod
+export!(show_store_offer_redirect_type);
+export!(simple_event_packet); // Mod
+export!(simulation_type);
+export!(social); // Mod
+export!(soft_enum_update_type);
+export!(spawn_biome_type);
+export!(spawn_position_type);
+export!(structure_block_type);
+export!(structure_redstone_save_mode);
+export!(structure_template_request_operation);
+export!(structure_template_response_type);
+export!(sub_chunk_packet); // Mod
+export!(tag); // Mod
+export!(text_packet_type);
+export!(text_processing_event_origin);
+export!(ui_profile);
+export!(persona); // Mod
