d3d82194d51f3cee35c2dace94db9eaa933aa983
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-22 01:02:20 +1300

    [enum] item_use_method.rs

---
+++ crates/proto/src/version/v662/enums/item_use_method.rs
@@ -0,0 +1,23 @@
+use bedrockrs_macros::ProtoCodec;
+
+#[derive(ProtoCodec)]
+pub enum ItemUseMethod {
+    Unknown = -1,
+    EquipArmor = 0,
+    Eat = 1,
+    Attack = 2,
+    Consume = 3,
+    Throw = 4,
+    Shoot = 5,
+    Place = 6,
+    FillBottle = 7,
+    FillBucket = 8,
+    PourBucket = 9,
+    UseTool = 10,
+    Interact = 11,
+    Retrieved = 12,
+    Dyed = 13,
+    Traded = 14,
+    BrushingCompleted = 15,
+    Count = 16,
+}
