43ac7e2db5fa6a8175f4b375773e323e951e6a26
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-22 02:36:36 +1300

    [enum] text_packet_type.rs

---
+++ crates/proto/src/version/v662/enums/text_packet_type.rs
@@ -0,0 +1,17 @@
+use bedrockrs_macros::ProtoCodec;
+
+#[derive(ProtoCodec)]
+pub enum TextPacketType {
+    Raw = 0,
+    Chat = 1,
+    Translate = 2,
+    Popup = 3,
+    JukeboxPopup = 4,
+    Tip = 5,
+    SystemMessage = 6,
+    Whisper = 7,
+    Announcement = 8,
+    TextObjectWhisper = 9,
+    TextObject = 10,
+    TextObjectAnnouncement = 11,
+}
