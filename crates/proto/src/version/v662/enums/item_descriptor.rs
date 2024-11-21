5bd74df2eb0581dc59c1f6e8510f159f3cdaf96c
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-22 00:31:40 +1300

    [enum] item_descriptor.rs (MOD)

---
+++ crates/proto/src/version/v662/enums/item_descriptor.rs
@@ -0,0 +1,13 @@
+pub mod ItemDescriptor {
+    use bedrockrs_macros::ProtoCodec;
+    
+    #[derive(ProtoCodec)]
+    pub enum InternalType {
+        Invalid = 0,
+        Default = 1,
+        Molang = 2,
+        ItemTag = 3,
+        Deferred = 4,
+        ComplexAlias = 5,
+    }
+}
