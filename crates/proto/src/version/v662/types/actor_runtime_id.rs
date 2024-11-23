f1ee7bfa89a3aa2fe20434c861337fbefc342f4d
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-24 00:43:50 +1300

    [type] actor_runtime_id.rs

---
+++ crates/proto/src/version/v662/types/actor_runtime_id.rs
@@ -0,0 +1,23 @@
+use std::io::Cursor;
+use std::mem::size_of;
+use varint_rs::{VarintReader, VarintWriter};
+use bedrockrs_proto_core::error::ProtoCodecError;
+use bedrockrs_proto_core::ProtoCodec;
+
+pub struct ActorRuntimeID(pub u64);
+
+impl ProtoCodec for ActorRuntimeID {
+    fn proto_serialize(&self, stream: &mut Vec<u8>) -> Result<(), ProtoCodecError> {
+        stream.write_u64_varint(self.0)?;
+
+        Ok(())
+    }
+
+    fn proto_deserialize(stream: &mut Cursor<&[u8]>) -> Result<Self, ProtoCodecError> {
+        Ok(Self(stream.read_u64_varint()?))
+    }
+
+    fn get_size_prediction(&self) -> usize {
+        size_of::<u64>()
+    }
+}
