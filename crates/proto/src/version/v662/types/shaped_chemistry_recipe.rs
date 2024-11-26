b2be57e8c112691ec33a160536fcea736b85bb8f
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-26 17:44:07 +1300

    [type] shaped_chemistry_recipe.rs

---
+++ crates/proto/src/version/v662/types/shaped_chemistry_recipe.rs
@@ -0,0 +1,20 @@
+use uuid::Uuid;
+use bedrockrs_macros::ProtoCodec;
+use crate::version::v662::types::{NetworkItemInstanceDescriptor, RecipeIngredient};
+
+#[derive(ProtoCodec)]
+pub struct ShapedChemistryRecipe {
+    pub recipe_id: String,
+    #[endianness(var)]
+    pub width: i32,
+    #[endianness(var)]
+    pub height: i32,
+    pub ingredient: RecipeIngredient,
+    #[vec_repr(u32)]
+    #[vec_endianness(var)]
+    pub result_items: Vec<NetworkItemInstanceDescriptor>,
+    pub id: Uuid,
+    pub tag: String,
+    #[endianness(var)]
+    pub priority: i32,
+}
