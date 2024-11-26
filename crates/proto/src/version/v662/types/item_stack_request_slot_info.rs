b062316a4d6b177412d52642d901a50203ff3159
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-26 17:39:39 +1300

    [type] item_stack_request_slot_info.rs

---
+++ crates/proto/src/version/v662/types/item_stack_request_slot_info.rs
@@ -0,0 +1,9 @@
+use bedrockrs_macros::ProtoCodec;
+
+#[derive(ProtoCodec)]
+pub struct ItemStackRequestSlotInfo {
+    pub container_net_id: i8,
+    pub slot: i8,
+    #[endianness(var)]
+    pub raw_id: i32,
+}
