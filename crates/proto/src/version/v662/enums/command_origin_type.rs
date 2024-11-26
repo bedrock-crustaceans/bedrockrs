b6067edcae6bb2aac249c62fc651311b15c5898a
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-26 17:35:46 +1300

    [enum] command_origin_type.rs

--- crates/proto/src/version/v662/enums/command_origin_type.rs
+++ crates/proto/src/version/v662/enums/command_origin_type.rs
@@ -1,12 +1,17 @@
 use bedrockrs_macros::ProtoCodec;
 
 #[derive(ProtoCodec)]
+#[enum_repr(u32)]
+#[enum_endianness(var)]
+#[repr(u32)]
 pub enum CommandOriginType {
     Player = 0,
     CommandBlock = 1,
     MinecartCommandBlock = 2,
-    DevConsole = 3,
-    Test = 4,
+    #[endianness(var)]
+    DevConsole(i64) = 3,
+    #[endianness(var)]
+    Test(i64) = 4,
     AutomationPlayer = 5,
     ClientAutomation = 6,
     DedicatedServer = 7,
