216d57f24a0bb03ab0aa88759bd4c07c14b0be71
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-26 17:51:44 +1300

    [enum] social.rs

--- crates/proto/src/version/v662/enums/social.rs
+++ crates/proto/src/version/v662/enums/social.rs
@@ -2,6 +2,9 @@ pub mod Social {
     use bedrockrs_macros::ProtoCodec;
     
     #[derive(ProtoCodec)]
+    #[enum_repr(i32)]
+    #[enum_endianness(var)]
+    #[repr(i32)]
     pub enum GamePublishSetting {
         NoMultiPlay = 0,
         InviteOnly = 1,
