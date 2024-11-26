6e1a79d1d0fbbc8cc229be2cf41dfa90332e0fa6
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-26 17:51:28 +1300

    [enum] education_edition_offer.rs

--- crates/proto/src/version/v662/enums/education_edition_offer.rs
+++ crates/proto/src/version/v662/enums/education_edition_offer.rs
@@ -3,6 +3,7 @@ use bedrockrs_macros::ProtoCodec;
 #[derive(ProtoCodec)]
 #[enum_repr(i32)]
 #[enum_endianness(var)]
+#[repr(i32)]
 pub enum EducationEditionOffer {
     None = 0,
     RestOfWorld = 1,
