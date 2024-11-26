5aef996a93e5b22bb1d490a7d55b6ecbbf51f45f
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-26 17:44:48 +1300

    [enum] spawn_biome_type.rs

--- crates/proto/src/version/v662/enums/spawn_biome_type.rs
+++ crates/proto/src/version/v662/enums/spawn_biome_type.rs
@@ -1,6 +1,9 @@
 use bedrockrs_macros::ProtoCodec;
 
 #[derive(ProtoCodec)]
+#[enum_repr(i16)]
+#[enum_endianness(le)]
+#[repr(i16)]
 pub enum SpawnBiomeType {
     Default = 0,
     UserDefined = 1,
