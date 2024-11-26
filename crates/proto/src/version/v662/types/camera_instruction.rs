a75cc92156e254c7afe23a9f9c992104294fc6a2
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-26 17:34:50 +1300

    [type] camera_instruction.rs

--- crates/proto/src/version/v662/types/camera_instruction.rs
+++ crates/proto/src/version/v662/types/camera_instruction.rs
@@ -2,7 +2,7 @@ use bedrockrs_macros::ProtoCodec;
 
 #[derive(ProtoCodec)]
 pub struct CameraInstruction{
-    pub set: Option<()>, // TODO: () --> struct CameraInstruction::SetInstruction
+    pub set: Option<()>, // TODO: CameraInstruction::SetInstruction
     pub clear: Option<bool>,
-    pub fade: Option<()>, // TODO: () --> struct CameraInstruction::FadeInstruction
+    pub fade: Option<()>, // TODO: CameraInstruction::FadeInstruction
 }
