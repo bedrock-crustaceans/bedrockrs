0e8a813f36abc33b0b327751ea4e90231c233361
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-11-26 17:43:56 +1300

    [enum] server_auth_movement_mode.rs

--- crates/proto/src/version/v662/enums/server_auth_movement_mode.rs
+++ crates/proto/src/version/v662/enums/server_auth_movement_mode.rs
@@ -1,6 +1,8 @@
 use bedrockrs_macros::ProtoCodec;
 
 #[derive(ProtoCodec)]
+#[enum_repr(i8)]
+#[repr(i8)]
 pub enum ServerAuthMovementMode {
     ClientAuthoritative = 0,
     ServerAuthoritative = 1,
