9ee29a7ab08b742469446eb9a865d23319017b88
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-01 21:18:19 +1300

    [packet] login.rs

--- crates/proto/src/version/v662/packets/login.rs
+++ crates/proto/src/version/v662/packets/login.rs
@@ -1,10 +1,9 @@
 use bedrockrs_macros::{gamepacket, ProtoCodec};
-use crate::version::v662::types::ConnectionRequest;
 
 #[gamepacket(id = 1)]
-#[derive(ProtoCodec, Debug, Clone)]
+#[derive(ProtoCodec)]
 pub struct LoginPacket {
     #[endianness(be)]
     pub client_network_version: i32,
-    pub connection_request: ConnectionRequest
+    pub connection_request: String,
 }
