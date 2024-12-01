4ff5fb97355877c1310c9df8ddcb27c85fe53b07
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-01 21:32:53 +1300

    set_actor_data.rs

--- crates/proto/src/version/v662/packets/set_actor_data.rs
+++ crates/proto/src/version/v662/packets/set_actor_data.rs
@@ -5,6 +5,8 @@ use crate::version::v662::types::{ActorRuntimeID, DataItem, PropertySyncData};
 #[derive(ProtoCodec)]
 pub struct SetActorDataPacket {
     pub target_runtime_id: ActorRuntimeID,
+    #[vec_repr(u32)]
+    #[vec_endianness(var)]
     pub actor_data: Vec<DataItem>, // TODO: Verify vec_repr & vec_endianness
     pub synced_properties: PropertySyncData,
     #[endianness(var)]
