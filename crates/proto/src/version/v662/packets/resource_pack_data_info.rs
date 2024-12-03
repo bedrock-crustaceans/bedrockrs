bd2212822fa2632272834749410ccec38a6948af
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-04 02:02:47 +1300

    [packet] resource_pack_data_info.rs

---
+++ crates/proto/src/version/v662/packets/resource_pack_data_info.rs
@@ -0,0 +1,17 @@
+use bedrockrs_macros::{gamepacket, ProtoCodec};
+use crate::version::v662::enums::PackType;
+
+#[gamepacket(id = 82)]
+#[derive(ProtoCodec)]
+pub struct ResourcePackDataInfoPacket {
+    pub resource_name: String,
+    #[endianness(le)]
+    pub chunk_size: u32,
+    #[endianness(le)]
+    pub chunk_amount: u32,
+    #[endianness(le)]
+    pub file_size: u64,
+    pub file_hash: String,
+    pub is_premium: bool,
+    pub pack_type: PackType,
+}
