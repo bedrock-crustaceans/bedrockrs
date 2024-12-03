96b852a738f75b75e39fc133bd3a81d553172cf9
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-04 02:01:17 +1300

    [packet] gui_data_pick_item.rs

---
+++ crates/proto/src/version/v662/packets/gui_data_pick_item.rs
@@ -0,0 +1,10 @@
+use bedrockrs_macros::{gamepacket, ProtoCodec};
+
+#[gamepacket(id = 54)]
+#[derive(ProtoCodec)]
+pub struct GuiDataPickItemPacket {
+    pub item_name: String,
+    pub item_effect_name: String,
+    #[endianness(le)]
+    pub slot: i32,
+}
