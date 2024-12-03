f6bc21ae91311304ec08032823c2a9e6a8313803
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-04 02:01:29 +1300

    [packet] level_chunk.rs

---
+++ crates/proto/src/version/v662/packets/level_chunk.rs
@@ -0,0 +1,18 @@
+use bedrockrs_macros::{gamepacket, ProtoCodec};
+use crate::version::v662::types::ChunkPos;
+
+#[gamepacket(id = 58)]
+#[derive(ProtoCodec)]
+pub struct LevelChunkPacket {
+    pub chunk_position: ChunkPos,
+    #[endianness(var)]
+    pub dimension_id: i32,
+    // TODO: sub-chunk count stuff
+    pub cache_enabled: bool,
+    #[vec_repr(u32)]
+    #[vec_endianness(var)]
+    pub cache_blobs: Vec<u64>,
+    pub serialized_chunk_data: String,
+}
+
+// TODO: this whole thing is terrible
