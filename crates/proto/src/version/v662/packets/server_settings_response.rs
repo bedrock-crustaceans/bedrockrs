69e0c2ba0ba92c583c3ffeecadf5e541871b4125
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-05 02:12:44 +1300

    [packet] server_settings_response.rs

---
+++ crates/proto/src/version/v662/packets/server_settings_response.rs
@@ -0,0 +1,11 @@
+use bedrockrs_macros::{gamepacket, ProtoCodec};
+
+#[gamepacket(id = 103)]
+#[derive(ProtoCodec)]
+pub struct ServerSettingsResponsePacket {
+    #[endianness(var)]
+    pub form_id: u32,
+    pub form_ui_json: String,
+}
+
+// TODO: this doesn't seem right. Probably an error in the proto docs. Check gopher or cloudburst
