f5b64f1dd8b219690800ba989eb199b1a21ea295
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-05 02:07:33 +1300

    [type] network_block_position.rs

--- crates/proto/src/version/v662/types/network_block_position.rs
+++ crates/proto/src/version/v662/types/network_block_position.rs
@@ -3,9 +3,9 @@ use bedrockrs_macros::ProtoCodec;
 #[derive(ProtoCodec)]
 pub struct NetworkBlockPosition {
     #[endianness(var)]
-    x: i32,
+    pub x: i32,
     #[endianness(var)]
-    y: u32,
+    pub y: u32,
     #[endianness(var)]
-    z: i32,
+    pub z: i32,
 }
