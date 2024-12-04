29b2d9f940823e9bd2ec478c556056e1f9597229
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-05 02:10:32 +1300

    [packet] emote.rs

---
+++ crates/proto/src/version/v662/packets/emote.rs
@@ -0,0 +1,20 @@
+use bedrockrs_macros::{gamepacket, ProtoCodec};
+use crate::version::v662::types::ActorRuntimeID;
+
+#[derive(ProtoCodec)]
+#[enum_repr(i8)]
+#[repr(i8)]
+enum Flags {
+    ServerSide = 1 << 0,
+    MuteEmoteChat = 1 << 1,
+}
+
+#[gamepacket(id = 138)]
+#[derive(ProtoCodec)]
+pub struct EmotePacket {
+    pub actor_runtime_id: ActorRuntimeID,
+    pub emote_id: String,
+    pub xuid: String,
+    pub platform_id: String,
+    pub flags: Flags,
+}
