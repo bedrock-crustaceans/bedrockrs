764ec3068172bbd13376b38dc07ecf79ad685c62
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-05 19:27:58 +1300

    [enum] molang_version.rs

--- crates/proto/src/version/v662/enums/molang_version.rs
+++ crates/proto/src/version/v662/enums/molang_version.rs
@@ -1,6 +1,9 @@
 use bedrockrs_macros::ProtoCodec;
 
 #[derive(ProtoCodec)]
+#[enum_repr(i32)]
+#[enum_endianness(le)]
+#[repr(i32)]
 pub enum MolangVersion {
     Invalid = -1,
     BeforeVersioning = 0,
@@ -20,6 +23,6 @@ pub enum MolangVersion {
 }
 
 impl MolangVersion {
-    const LATEST: MolangVersion = MolangVersion::NumValidVersions - 1;
+    const LATEST: MolangVersion = MolangVersion::LeafSupportingInFirstSolidBlockBelow; // TODO: NumValidVersions - 1 (error)
     const HARDCODED_MOLANG: MolangVersion = MolangVersion::LATEST;
 }
