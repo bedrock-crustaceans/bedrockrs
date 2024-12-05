c6c80b0ae369808278d0ed40f244f8432528ec0a
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-05 19:27:33 +1300

    [enum] inventory_right_tab_index.rs

--- crates/proto/src/version/v662/enums/inventory_right_tab_index.rs
+++ crates/proto/src/version/v662/enums/inventory_right_tab_index.rs
@@ -1,6 +1,9 @@
 use bedrockrs_macros::ProtoCodec;
 
 #[derive(ProtoCodec)]
+#[enum_repr(i32)]
+#[enum_endianness(var)]
+#[repr(i32)]
 pub enum InventoryRightTabIndex {
     None = 0,
     FullScreen = 1,
