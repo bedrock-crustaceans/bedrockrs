334522dcb1710e7fcff409c0033e7935ad33ed9b
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-09 02:38:47 +1300

    rem

--- crates/proto/src/version/v662/enums/mod.rs
+++ crates/proto/src/version/v662/enums/mod.rs
@@ -14,7 +14,6 @@ export!(actor_flags);
 export!(actor_link_type);
 export!(actor_type);
 export!(agent_action_type);
-export!(animate_packet); // Mod
 export!(animation_mode);
 export!(attribute_modifier_operation);
 export!(attribute_operands);
@@ -26,8 +25,6 @@ export!(camera_shake_action);
 export!(camera_shake_type);
 export!(chat_restriction_level);
 export!(client_play_mode);
-export!(clientbound_debug_renderer_packet); // Mod
-export!(clientbound_map_item_data_packet); // Mod
 export!(code_builder_storage_query_options); // Mod
 export!(command_block_mode);
 export!(command_origin_type);
@@ -46,7 +43,6 @@ export!(difficulty);
 export!(easing_type);
 export!(editor); // Mod
 export!(education_edition_offer);
-export!(emote_packet); // Mod
 export!(enchant); // Mod
 export!(game_rule); // Mod
 export!(game_type);
@@ -55,7 +51,6 @@ export!(hud_element);
 export!(hud_visibility);
 export!(identity_definition); // Mod
 export!(input_mode);
-export!(interact_packet); // Mod
 export!(inventory_layout);
 export!(inventory_left_tab_index);
 export!(inventory_right_tab_index);
@@ -68,7 +63,6 @@ export!(item_stack_request_action_type);
 export!(item_use_inventory_transaction); // Mod
 export!(item_use_method);
 export!(item_use_on_actor_inventory_transaction); // Mod
-export!(lab_table_packet); // Mod
 export!(lab_table_reaction_type);
 export!(legacy_telemetry_event_packet); // Mod
 export!(lesson_action);
@@ -78,13 +72,10 @@ export!(map_item_tracked_actor); // Mod
 export!(minecraft_eventing); // Mod
 export!(minecraft_packet_ids);
 export!(mirror);
-export!(mob_effect_packet); // Mod
 export!(modal_form_cancel_reason);
 export!(molang_version);
 export!(multiplayer_settings_packet_type);
 export!(new_interaction_model);
-export!(npc_dialogue_packet); // Mod
-export!(npc_request_packet); // Mod
 export!(objective_sort_order);
 export!(pack_type);
 export!(packet_compression_algorithm);
@@ -94,25 +85,18 @@ export!(particle_type);
 export!(photo_type);
 export!(play_status);
 export!(player_action_type);
-export!(player_auth_input_packet); // Mod
 export!(player_list_packet_type);
 export!(player_permission_level);
 export!(player_position_mode_component); // Mod
 export!(player_respawn_state);
-export!(position_tracking_db_client_request_packet); // Mod
-export!(position_tracking_db_server_broadcast_packet); // Mod
 export!(puv); // Mod
-export!(request_ability_packet); // Mod
 export!(resource_pack_response);
 export!(rotation);
 export!(score_packet_type);
 export!(scoreboard_identity_packet_type);
 export!(serialized_abilities_data); // Mod
 export!(server_auth_movement_mode);
-export!(set_title_packet); // Mod
-export!(show_credits_packet); // Mod
 export!(show_store_offer_redirect_type);
-export!(simple_event_packet); // Mod
 export!(simulation_type);
 export!(social); // Mod
 export!(soft_enum_update_type);
@@ -122,7 +106,6 @@ export!(structure_block_type);
 export!(structure_redstone_save_mode);
 export!(structure_template_request_operation);
 export!(structure_template_response_type);
-export!(sub_chunk_packet); // Mod
 export!(tag); // Mod
 export!(text_packet_type);
 export!(text_processing_event_origin);
