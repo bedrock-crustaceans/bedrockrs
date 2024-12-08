23b019a0ff81ca63c819f36462d1b24523375585
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-09 02:41:17 +1300

    impl ProtoCodec, VERIFY

--- crates/proto/src/version/v662/packets/animate.rs
+++ crates/proto/src/version/v662/packets/animate.rs
@@ -1,31 +1,71 @@
-use bedrockrs_macros::{gamepacket, ProtoCodec};
 use crate::version::v662::types::ActorRuntimeID;
+use bedrockrs_macros::{gamepacket, ProtoCodec};
+use bedrockrs_proto_core::error::ProtoCodecError;
+use bedrockrs_proto_core::ProtoCodec;
+use futures::AsyncWriteExt;
+use std::io::{Cursor, Read};
+use std::mem::size_of;
+use tokio::io::AsyncReadExt;
+use varint_rs::{VarintReader, VarintWriter};
 
 #[derive(ProtoCodec)]
 #[enum_repr(i32)]
 #[enum_endianness(var)]
 #[repr(i32)]
 enum Action {
-  NoAction = 0,
-  Swing = 1,
-  WakeUp = 3,
-  CriticalHit = 4,
-  MagicCriticalHit = 5,
-  RowRight {
-    #[endianness(le)]
-    rowing_time: f32,
-  } = 128,
-  RowLeft {
-    #[endianness(le)]
-    rowing_time: f32,
-  } = 129,
+    NoAction = 0,
+    Swing = 1,
+    WakeUp = 3,
+    CriticalHit = 4,
+    MagicCriticalHit = 5,
+    RowRight {
+        #[endianness(le)]
+        rowing_time: f32,
+    } = 128,
+    RowLeft {
+        #[endianness(le)]
+        rowing_time: f32,
+    } = 129,
 }
 
 #[gamepacket(id = 44)]
-#[derive(ProtoCodec)]
 pub struct AnimatePacket {
-  pub action: Action,
-  pub target_runtime_id: ActorRuntimeID,
+    pub action: Action,
+    pub target_runtime_id: ActorRuntimeID,
 }
 
-// TODO: custom proto impl, enum variant
+impl ProtoCodec for AnimatePacket {
+    fn proto_serialize(&self, stream: &mut Vec<u8>) -> Result<(), ProtoCodecError> {
+        let mut action_stream: Vec<u8> = Vec::new();
+        <Action as ProtoCodec>::proto_serialize(&self.action, &mut action_stream)?;
+        let mut action_cursor = Cursor::new(action_stream.as_slice());
+
+        stream.write_i32_varint(action_cursor.read_i32_varint()?)?;
+        <ActorRuntimeID as ProtoCodec>::proto_serialize(&self.target_runtime_id, stream)?;
+        action_cursor.read_to_end(stream)?;
+
+        Ok(())
+    }
+
+    fn proto_deserialize(stream: &mut Cursor<&[u8]>) -> Result<Self, ProtoCodecError> {
+        let mut action_stream: Vec<u8> = Vec::new();
+
+        action_stream.write_i32_varint(stream.read_i32_varint()?)?;
+        let target_runtime_id = <ActorRuntimeID as ProtoCodec>::proto_deserialize(stream)?;
+        stream.read_to_end(action_stream)?;
+
+        let mut action_cursor = Cursor::new(action_stream.as_slice());
+        let action = <Action as ProtoCodec>::proto_deserialize(&mut action_cursor)?;
+
+        Ok(Self {
+            action,
+            target_runtime_id,
+        })
+    }
+
+    fn get_size_prediction(&self) -> usize {
+        self.action.get_size_prediction() + self.target_runtime_id.get_size_prediction()
+    }
+}
+
+// VERIFY: ProtoCodec impl
