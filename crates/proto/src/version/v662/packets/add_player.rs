541924cf3641ed72bc28682de9c7cf314337e5fd
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-09 02:40:46 +1300

    repl Vec3 & Vec2, TODO -> VERIFY

--- crates/proto/src/version/v662/packets/add_player.rs
+++ crates/proto/src/version/v662/packets/add_player.rs
@@ -1,7 +1,8 @@
 use uuid::Uuid;
+use bedrockrs_core::{Vec2, Vec3};
 use bedrockrs_macros::{gamepacket, ProtoCodec};
 use crate::version::v662::enums::{BuildPlatform, GameType};
-use crate::version::v662::types::{ActorLink, ActorRuntimeID, DataItem, NetworkItemStackDescriptor, PropertySyncData, SerializedAbilitiesData, Vec2, Vec3};
+use crate::version::v662::types::{ActorLink, ActorRuntimeID, DataItem, NetworkItemStackDescriptor, PropertySyncData, SerializedAbilitiesData};
 
 #[gamepacket(id = 12)]
 #[derive(ProtoCodec)]
@@ -10,16 +11,19 @@ pub struct AddPlayerPacket {
     pub player_name: String,
     pub target_runtime_id: ActorRuntimeID,
     pub platform_chat_id: String,
-    pub position: Vec3,
-    pub velocity: Vec3,
-    pub rotation: Vec2,
+    #[endianness(le)]
+    pub position: Vec3<f32>,
+    #[endianness(le)]
+    pub velocity: Vec3<f32>,
+    #[endianness(le)]
+    pub rotation: Vec2<f32>,
     #[endianness(le)]
     pub y_head_rotation: f32,
     pub carried_item: NetworkItemStackDescriptor,
     pub player_game_type: GameType,
     #[vec_repr(u32)]
     #[vec_endianness(var)]
-    pub entity_data: Vec<DataItem>, // TODO: Verify vec_repr & vec_endianness
+    pub entity_data: Vec<DataItem>, // VERIFY: vec_repr & vec_endianness
     pub synced_properties: PropertySyncData,
     pub abilities_data: SerializedAbilitiesData,
     #[vec_repr(u32)]
