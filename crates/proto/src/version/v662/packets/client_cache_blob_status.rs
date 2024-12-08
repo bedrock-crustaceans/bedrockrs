1b20436403cacbca172b156f3de4c45cf2916e21
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-09 02:42:04 +1300

    impl ProtoCodec, VERIFY

--- crates/proto/src/version/v662/packets/client_cache_blob_status.rs
+++ crates/proto/src/version/v662/packets/client_cache_blob_status.rs
@@ -1,7 +1,10 @@
 use bedrockrs_macros::{gamepacket, ProtoCodec};
+use bedrockrs_proto_core::error::ProtoCodecError;
+use bedrockrs_proto_core::{ProtoCodec, ProtoCodecLE, ProtoCodecVAR};
+use std::io::Cursor;
+use std::mem::size_of;
 
 #[gamepacket(id = 135)]
-#[derive(ProtoCodec)]
 pub struct ClientCacheBlobStatusPacket {
     #[vec_repr(u32)]
     #[vec_endianness(var)]
@@ -11,4 +14,44 @@ pub struct ClientCacheBlobStatusPacket {
     pub obtained_blobs: Vec<u64>,
 }
 
-// TODO: custom proto impl, both Vec lengths come first in packet, then both their elements, respectfully. (elements are le)
+impl ProtoCodec for ClientCacheBlobStatusPacket {
+    fn proto_serialize(&self, stream: &mut Vec<u8>) -> Result<(), ProtoCodecError> {
+        <u32 as ProtoCodecVAR>::proto_serialize(self.missing_blobs.len().try_into()?, stream)?;
+        <u32 as ProtoCodecVAR>::proto_serialize(self.obtained_blobs.len().try_into()?, stream)?;
+        for i in &self.missing_blobs {
+            <u64 as ProtoCodecLE>::proto_serialize(i, stream)?;
+        }
+        for i in &self.obtained_blobs {
+            <u64 as ProtoCodecLE>::proto_serialize(i, stream)?;
+        }
+
+        Ok(())
+    }
+
+    fn proto_deserialize(stream: &mut Cursor<&[u8]>) -> Result<Self, ProtoCodecError> {
+        let missing_blobs_len = <u32 as ProtoCodecVAR>::proto_deserialize(stream)?;
+        let obtained_blobs_len = <u32 as ProtoCodecVAR>::proto_deserialize(stream)?;
+        let mut missing_blobs = Vec::with_capacity(missing_blobs_len.try_into()?);
+        let mut obtained_blobs = Vec::with_capacity(obtained_blobs_len.try_into()?);
+        for _ in 0..missing_blobs_len {
+            missing_blobs.push(<u64 as ProtoCodecLE>::proto_deserialize(stream)?);
+        }
+        for _ in 0..obtained_blobs_len {
+            obtained_blobs.push(<u64 as ProtoCodecLE>::proto_deserialize(stream)?);
+        }
+
+        Ok(Self {
+            missing_blobs,
+            obtained_blobs,
+        })
+    }
+
+    fn get_size_prediction(&self) -> usize {
+        size_of::<u32>()
+            + size_of::<u32>()
+            + self.missing_blobs.len() * size_of::<u64>()
+            + self.obtained_blobs.len() * size_of::<u64>()
+    }
+}
+
+// VERIFY: ProtoCodec impl
