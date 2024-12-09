ce099a4c2628bec8f859d049bfefdf9c728e39f7
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-09 16:21:15 +1300

    repl Vec3

--- crates/proto/src/version/v662/packets/motion_prediction_hints.rs
+++ crates/proto/src/version/v662/packets/motion_prediction_hints.rs
@@ -1,10 +1,12 @@
+use bedrockrs_core::Vec3;
 use bedrockrs_macros::{gamepacket, ProtoCodec};
-use crate::version::v662::types::{ActorRuntimeID, Vec3};
+use crate::version::v662::types::ActorRuntimeID;
 
 #[gamepacket(id = 157)]
 #[derive(ProtoCodec)]
 pub struct MotionPredictionHintsPacket {
     pub runtime_id: ActorRuntimeID,
-    pub motion: Vec3,
+    #[endianness(le)]
+    pub motion: Vec3<f32>,
     pub on_ground: bool,
 }
