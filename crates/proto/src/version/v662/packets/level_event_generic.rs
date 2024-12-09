6c551bdec8d2ed01a727b4ff1c8b8d7561c40aea
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-09 16:20:48 +1300

    repl CompoundTag

--- crates/proto/src/version/v662/packets/level_event_generic.rs
+++ crates/proto/src/version/v662/packets/level_event_generic.rs
@@ -1,10 +1,10 @@
 use bedrockrs_macros::{gamepacket, ProtoCodec};
 use crate::version::v662::enums::LevelEvent;
-use crate::version::v662::types::CompoundTag;
 
 #[gamepacket(id = 124)]
 #[derive(ProtoCodec)]
 pub struct LevelEventGenericPacket {
     pub event_id: LevelEvent,
-    pub event_data: CompoundTag,
+    #[nbt]
+    pub event_data: nbtx::Value, // TODO: NBT Structure
 }
