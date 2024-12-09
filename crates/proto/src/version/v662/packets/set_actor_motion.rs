b85d4c09dce30db4911323b16cb2c4490d650729
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-09 16:22:08 +1300

    repl Vec3

--- crates/proto/src/version/v662/packets/set_actor_motion.rs
+++ crates/proto/src/version/v662/packets/set_actor_motion.rs
@@ -1,11 +1,13 @@
+use bedrockrs_core::Vec3;
 use bedrockrs_macros::{gamepacket, ProtoCodec};
-use crate::version::v662::types::{ActorRuntimeID, Vec3};
+use crate::version::v662::types::ActorRuntimeID;
 
 #[gamepacket(id = 40)]
 #[derive(ProtoCodec)]
 pub struct SetActorMotionPacket {
     pub target_runtime_id: ActorRuntimeID,
-    pub motion: Vec3,
+    #[endianness(le)]
+    pub motion: Vec3<f32>,
     #[endianness(var)]
     pub server_tick: u64,
 }
