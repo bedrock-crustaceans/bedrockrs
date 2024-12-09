357128dcec0c1f51cbb10b621f8fd61dc061e243
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-09 16:21:57 +1300

    repl Vec3

--- crates/proto/src/version/v662/packets/server_player_post_move_position.rs
+++ crates/proto/src/version/v662/packets/server_player_post_move_position.rs
@@ -1,8 +1,9 @@
+use bedrockrs_core::Vec3;
 use bedrockrs_macros::{gamepacket, ProtoCodec};
-use crate::version::v662::types::Vec3;
 
 #[gamepacket(id = 16)]
 #[derive(ProtoCodec)]
 pub struct ServerPlayerPostMovePositionPacket {
-    pub pos: Vec3,
+    #[endianness(le)]
+    pub pos: Vec3<f32>,
 }
